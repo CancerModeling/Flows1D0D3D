b6ea409d3cfc92ddc61596fb8729c18d813147c7
Prashant Jha <prashjha@login2.stampede2.tacc.utexas.edu>
2020-06-03 14:07:01 -0500

    changes needed to run on stampede

--- cmake/FindPetsc.cmake
+++ cmake/FindPetsc.cmake
@@ -4,11 +4,11 @@
 # (See accompanying file LICENSE.txt)
 find_package(PkgConfig)
 
-find_library(PETSC_LIB
+find_library(PETSC_LIBRARIES
         NAMES libpetsc.so libpetsc.dylib
-        HINTS /usr/lib64 /usr/local/lib64 /usr/lib/ /usr/local/lib "${PETSC_DIR}/lib/")
+        HINTS /usr/lib64 /usr/local/lib64 /usr/lib/ /usr/local/lib "${PETSC_LIB}")
 
 
-if (NOT PETSC_LIB)
-    message(FATAL_ERROR "PETSC_LIB Library not found: Specify the PETSC_DIR where petsc is located")
+if (NOT PETSC_LIBRARIES)
+    message(FATAL_ERROR "PETSC_LIBRARIES Library not found: Specify the PETSC_DIR where petsc is located")
 endif ()
