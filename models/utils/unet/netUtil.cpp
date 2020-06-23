454da1b8db8bc17ca7ae44123f16a00884aee661
koepplto <41584027+koepplto@users.noreply.github.com>
2020-06-23 21:24:26 +0200

    Update Tobias Growth algorithm

--- models/utils/unet/netUtil.cpp
+++ models/utils/unet/netUtil.cpp
@@ -177,325 +177,330 @@ std::vector<double> util::unet::getCenterFace(std::vector<double> center,
                                                 std::vector<double> direction,
                                                 double h_3D) {
 
-  std::vector<double> center_face;
+                    std::vector<double> center_face;
 
-  for (int i = 0; i < 3; i++) {
+                    for (int i = 0; i < 3; i++) {
 
-    center_face.push_back(center[i] + 0.5 * h_3D * direction[i]);
-  }
+                         center_face.push_back(center[i] + 0.5 * h_3D * direction[i]);
+                    }
 
-  return center_face;
+                    return center_face;
+
 }
 
 double util::unet::getDirichletValue(std::vector<double> center_face,
                                        double L_p, double radius) {
 
-  double dirichlet_value = 0.0;
+       double dirichlet_value = 0.0;
 
-  double dist = (center_face[0] - 0.5) * (center_face[0] - 0.5) +
-                (center_face[1] - 0.5) * (center_face[1] - 0.5);
+       double dist = (center_face[0] - 0.5) * (center_face[0] - 0.5) + ( center_face[1] - 0.5) * (center_face[1] - 0.5);
 
-  dist = std::sqrt(dist);
+       dist = std::sqrt(dist);
 
-  if (dist < radius) {
+       if (dist < radius) {
 
-    dirichlet_value = (1.0 + center_face[2]) * L_p / (1.0 + L_p);
+           dirichlet_value = (1.0 + center_face[2]) * L_p / (1.0 + L_p);
 
-  } else {
+       } 
+       else {
 
-    dirichlet_value = (1.0 + center_face[2]) * L_p / (1.0 + L_p) *
+           dirichlet_value = (1.0 + center_face[2]) * L_p / (1.0 + L_p) *
                       (1.0 - radius * std::log(dist / radius));
-  }
+       }
 
-  return dirichlet_value;
+       return dirichlet_value;
+
 }
 
 int util::unet::getIndex(double value, double h_3D) {
 
-  int index = 0;
+    int index = 0;
 
-  while (index * h_3D < value - 1.0e-8) {
+    while (index * h_3D < value - 1.0e-8) {
 
-    index = index + 1;
-  }
+           index = index + 1;
+    }
 
-  return index;
+    return index;
+
 }
 
 double util::unet::normVector(std::vector<double> &vec) {
 
-  double norm_vec = 0.0;
+       double norm_vec = 0.0;
 
-  for (int j = 0; j < 3; j++) {
+       for (int j = 0; j < 3; j++) {
 
-    norm_vec = norm_vec + vec[j] * vec[j];
-  }
+            norm_vec = norm_vec + vec[j] * vec[j];
+       }
 
-  norm_vec = std::sqrt(norm_vec);
+       norm_vec = std::sqrt(norm_vec);
 
-  for (int j = 0; j < 3; j++) {
+       for (int j = 0; j < 3; j++) {
 
-    vec[j] = vec[j] / norm_vec;
-  }
+            vec[j] = vec[j] / norm_vec;
 
-  return norm_vec;
+       }
+
+       return norm_vec;
+
 }
 
 std::vector<double> util::unet::determineRotator(std::vector<double> dir) {
 
-  std::vector<double> rotator;
+                    std::vector<double> rotator;
 
-  if (std::abs(dir[0]) < 1.0e-13 && std::abs(dir[1]) < 1.0e-13) {
+                    if (std::abs(dir[0]) < 1.0e-13 && std::abs(dir[1]) < 1.0e-13) {
 
-    rotator.push_back(1.0);
+                        rotator.push_back(1.0);
+                        rotator.push_back(0.0);
+                        rotator.push_back(0.0);
 
-    rotator.push_back(0.0);
+                    } 
+                    else {
 
-    rotator.push_back(0.0);
+                        rotator.push_back(-dir[1]);
+                        rotator.push_back(dir[0]);
+                        rotator.push_back(0.0);
 
-  } else {
+                    }
 
-    rotator.push_back(-dir[1]);
+                    double length_rotator = normVector( rotator );   
 
-    rotator.push_back(dir[0]);
+                    for(int j=0;j<3;j++){ 
+                     
+                        rotator[ j ] = rotator[ j ]/length_rotator;
 
-    rotator.push_back(0.0);
-  }
+                    }
 
-  double length_rotator = normVector(rotator);
+                    return rotator;
 
-  for (int j = 0; j < 3; j++) {
-
-    rotator[j] = rotator[j] / length_rotator;
-  }
-
-  return rotator;
 }
 
-std::vector<double> util::unet::computeNodesOnCylinders(
-    std::vector<double> dir, std::vector<double> rotator,
-    std::vector<double> midpoint, double radius, double theta) {
+std::vector<double> util::unet::computeNodesOnCylinders( std::vector<double> dir,
+                                                         std::vector<double> rotator,
+                                                         std::vector<double> midpoint,
+                                                         double radius, double theta ) {
 
-  std::vector<double> cylinder_node(3);
+                    std::vector<double> cylinder_node(3);
 
-  double c = std::cos(theta);
+                    double c = std::cos(theta);
+                    double s = std::sin(theta);
 
-  double s = std::sin(theta);
+                    gmm::row_matrix<std::vector<double>> R(3, 3);
 
-  gmm::row_matrix<std::vector<double>> R(3, 3);
+		    R(0, 0) = dir[0] * dir[0] * (1.0 - c) + c;
+		    R(1, 1) = dir[1] * dir[1] * (1.0 - c) + c;
+		    R(2, 2) = dir[2] * dir[2] * (1.0 - c) + c;
+		    R(0, 1) = dir[0] * dir[1] * (1.0 - c) - dir[2] * s;
+		    R(0, 2) = dir[0] * dir[2] * (1.0 - c) + dir[1] * s;
+		    R(1, 0) = dir[0] * dir[1] * (1.0 - c) + dir[2] * s;
+		    R(2, 0) = dir[0] * dir[2] * (1.0 - c) - dir[1] * s;
+		    R(1, 2) = dir[1] * dir[2] * (1.0 - c) - dir[0] * s;
+		    R(2, 1) = dir[1] * dir[2] * (1.0 - c) + dir[0] * s;
 
-  R(0, 0) = dir[0] * dir[0] * (1.0 - c) + c;
-  R(1, 1) = dir[1] * dir[1] * (1.0 - c) + c;
-  R(2, 2) = dir[2] * dir[2] * (1.0 - c) + c;
-  R(0, 1) = dir[0] * dir[1] * (1.0 - c) - dir[2] * s;
-  R(0, 2) = dir[0] * dir[2] * (1.0 - c) + dir[1] * s;
-  R(1, 0) = dir[0] * dir[1] * (1.0 - c) + dir[2] * s;
-  R(2, 0) = dir[0] * dir[2] * (1.0 - c) - dir[1] * s;
-  R(1, 2) = dir[1] * dir[2] * (1.0 - c) - dir[0] * s;
-  R(2, 1) = dir[1] * dir[2] * (1.0 - c) + dir[0] * s;
+		    gmm::mult(R, rotator, cylinder_node);
+		    gmm::scale(cylinder_node, radius);
+		    gmm::add(midpoint, cylinder_node);
 
-  gmm::mult(R, rotator, cylinder_node);
+		    return cylinder_node;
 
-  gmm::scale(cylinder_node, radius);
-
-  gmm::add(midpoint, cylinder_node);
-
-  return cylinder_node;
 }
 
 void util::unet::updateWeightsAndIds(int N_s, int N_theta, int elementIndex,
-                                       std::vector<double> &weights,
-                                       std::vector<int> &id_3D_elements) {
+                                     std::vector<double> &weights,
+                                     std::vector<int> &id_3D_elements) {
 
-  bool elementfound = false;
+     bool elementfound = false;
 
-  int indexfound = 0;
+     int indexfound = 0;
+     int numberOfElements = id_3D_elements.size();
 
-  int numberOfElements = id_3D_elements.size();
+     for (int i = 0; i < numberOfElements; i++) {
 
-  for (int i = 0; i < numberOfElements; i++) {
+          if (elementIndex == id_3D_elements[i]) {
 
-    if (elementIndex == id_3D_elements[i]) {
+              indexfound = i;
 
-      indexfound = i;
+              elementfound = true;
+              break;
+          }
 
-      elementfound = true;
-      break;
-    }
-  }
+     }
 
-  if (elementfound == true) {
+     if (elementfound == true) {
 
-    weights[indexfound] = weights[indexfound] + 1.0 / (N_s * N_theta);
+         weights[indexfound] = weights[indexfound] + 1.0 / (N_s * N_theta);
 
-  } else {
+     }
+     else {
 
-    id_3D_elements.push_back(elementIndex);
-    weights.push_back(1.0 / (N_s * N_theta));
-  }
+         id_3D_elements.push_back(elementIndex);
+         weights.push_back(1.0 / (N_s * N_theta));
+     }
+
 }
 
-void util::unet::determineWeightsAndIds(int N_s, int N_theta, int N_3D,
-                                          std::vector<double> coord,
-                                          std::vector<double> coord_neighbor,
-                                          double radius, double h_3D,
-                                          double &length_edge,
-                                          std::vector<double> &weights,
-                                          std::vector<int> &id_3D_elements) {
+void util::unet::determineWeightsAndIds(int N_s, int N_theta, int N_3D, std::vector<double> coord, std::vector<double> coord_neighbor,
+                                        double radius, double h_3D, double length_edge, std::vector<double> &weights, std::vector<int> &id_3D_elements) {
 
-  std::vector<double> direction, rotator;
+     std::vector<double> direction, rotator;
 
-  for (int j = 0; j < 3; j++) {
+     for(int j=0;j<3;j++){
+  
+         direction.push_back( coord_neighbor[ j ] - coord[ j ] );
 
-    direction.push_back(coord_neighbor[j] - coord[j]);
-  }
+     }
 
-  length_edge = normVector(direction);
+     rotator = determineRotator( direction );
 
-  rotator = determineRotator(direction);
+     double length_rotator = normVector( rotator );
 
-  double length_rotator = normVector(rotator);
+     for(int i_s=1;i_s<N_s-1;i_s++){
 
-  for (int i_s = 1; i_s < N_s - 1; i_s++) {
+         std::vector<double> midpoint(3);
 
-    std::vector<double> midpoint(3);
+         double theta = 0.0;
 
-    double theta = 0.0;
+         for(int j=0;j<3;j++){
 
-    for (int j = 0; j < 3; j++) {
+             midpoint[ j ] = coord[ j ] + ( ( (double) i_s/(double) N_s ) * (length_edge) ) * direction[ j ];
 
-      midpoint[j] =
-          coord[j] +
-          (((double)i_s / (double)N_s) * (0.5 * length_edge)) * direction[j];
-    }
+         }
 
-    for (int i_theta = 0; i_theta < N_theta; i_theta++) {
+         for(int i_theta=0;i_theta<N_theta;i_theta++){
 
-      theta = ((double)i_theta) / ((double)N_theta) * 2.0 * M_PI;
+             theta = ((double) i_theta)/((double) N_theta)*2.0*M_PI;
 
-      std::vector<double> cylinder_node =
-          computeNodesOnCylinders(direction, rotator, midpoint, radius, theta);
+             std::vector<double> cylinder_node = computeNodesOnCylinders( direction,  rotator, midpoint,  radius, theta );
 
-      int elementIndex = getElementIndex(cylinder_node, h_3D, N_3D);
+             if( isCenterInDomain( cylinder_node, 2.0 ) ){
 
-      // Compute weights and element ids
-      updateWeightsAndIds(N_s - 2, N_theta, elementIndex, weights,
-                          id_3D_elements);
-    }
-  }
+                 int elementIndex = getElementIndex( cylinder_node, h_3D, N_3D );
+
+                 // Compute weights and element ids
+                 updateWeightsAndIds( N_s-2, N_theta, elementIndex, weights, id_3D_elements );
+
+             }
+
+         }
+
+     }
+
 }
 
-std::vector<double> util::unet::getCenterFromIndex(int index, int N_3D,
-                                                     double h_3D) {
+std::vector<double> util::unet::getCenterFromIndex(int index, int N_3D, double h_3D) {
 
-  std::vector<double> center = std::vector<double>(3, 0.0);
+                    std::vector<double> center = std::vector<double>(3,0.0);
 
-  int k = index / (N_3D * N_3D);
+                    int k = index/( N_3D*N_3D );
 
-  index = index - (k * N_3D * N_3D);
+                    index = index - ( k*N_3D*N_3D );
 
-  int j = index / N_3D;
+                    int j = index/N_3D;
 
-  int i = index - (j * N_3D);
+                    int i = index - ( j*N_3D );
 
-  center[0] = (double)i * h_3D + 0.5 * h_3D;
-  center[1] = (double)j * h_3D + 0.5 * h_3D;
-  center[2] = (double)k * h_3D + 0.5 * h_3D;
+                    center[ 0 ] = (double)i * h_3D + 0.5 * h_3D;
+                    center[ 1 ] = (double)j * h_3D + 0.5 * h_3D;
+                    center[ 2 ] = (double)k * h_3D + 0.5 * h_3D; 
 
-  return center;
+                    return center;
+
 }
+std::vector<int> util::unet::getNeighboringElementIndices( int index, int N_3D, double h_3D, double L_x ){
 
-std::vector<int> util::unet::getNeighboringElementIndices(int index, int N_3D,
-                                                            double h_3D,
-                                                            double L_x) {
+                 std::vector<int> indicesNeighbors;
 
-  std::vector<int> indicesNeighbors;
+                 std::vector<int> directions;
 
-  std::vector<int> directions;
+                 directions.push_back( -2 );
+                 directions.push_back(  2 );
 
-  directions.push_back(-2);
-  directions.push_back(2);
+                 directions.push_back( -1 );
+                 directions.push_back(  1 );
 
-  directions.push_back(-1);
-  directions.push_back(1);
+                 directions.push_back( -N_3D );
+                 directions.push_back(  N_3D );
 
-  directions.push_back(-N_3D);
-  directions.push_back(N_3D);
+                 directions.push_back( -2*N_3D );
+                 directions.push_back(  2*N_3D );
 
-  directions.push_back(-2 * N_3D);
-  directions.push_back(2 * N_3D);
+                 directions.push_back(  N_3D-1 );
+                 directions.push_back(  N_3D+1 );
 
-  directions.push_back(N_3D - 1);
-  directions.push_back(N_3D + 1);
+                 directions.push_back(  2*N_3D-1 );
+                 directions.push_back(  2*N_3D+1 );
 
-  directions.push_back(2 * N_3D - 1);
-  directions.push_back(2 * N_3D + 1);
+                 directions.push_back( -2*N_3D-1 );
+                 directions.push_back(  2*N_3D+1 );
 
-  directions.push_back(-2 * N_3D - 1);
-  directions.push_back(2 * N_3D + 1);
+                 directions.push_back( -N_3D-1 );
+                 directions.push_back( -N_3D+1 );
 
-  directions.push_back(-N_3D - 1);
-  directions.push_back(-N_3D + 1);
+                 directions.push_back( -1+( N_3D*N_3D ) );
+                 directions.push_back(  1+( N_3D*N_3D ) );
 
-  directions.push_back(-1 + (N_3D * N_3D));
-  directions.push_back(1 + (N_3D * N_3D));
+                 directions.push_back( -1+( 2*N_3D*N_3D ) );
+                 directions.push_back(  1+( 2*N_3D*N_3D ) );
 
-  directions.push_back(-1 + (2 * N_3D * N_3D));
-  directions.push_back(1 + (2 * N_3D * N_3D));
+                 directions.push_back( -N_3D+( N_3D*N_3D ) );
+                 directions.push_back(  N_3D+( N_3D*N_3D ) );
 
-  directions.push_back(-N_3D + (N_3D * N_3D));
-  directions.push_back(N_3D + (N_3D * N_3D));
+                 directions.push_back( -N_3D+( 2*N_3D*N_3D ) );
+                 directions.push_back(  N_3D+( 2*N_3D*N_3D ) );
 
-  directions.push_back(-N_3D + (2 * N_3D * N_3D));
-  directions.push_back(N_3D + (2 * N_3D * N_3D));
+                 directions.push_back(  N_3D-1+( N_3D*N_3D ) );
+                 directions.push_back(  N_3D+1+( N_3D*N_3D ) );
 
-  directions.push_back(N_3D - 1 + (N_3D * N_3D));
-  directions.push_back(N_3D + 1 + (N_3D * N_3D));
+                 directions.push_back(  N_3D-1+2*( N_3D*N_3D ) );
+                 directions.push_back(  N_3D+1+2*( N_3D*N_3D ) );
 
-  directions.push_back(N_3D - 1 + 2 * (N_3D * N_3D));
-  directions.push_back(N_3D + 1 + 2 * (N_3D * N_3D));
+                 directions.push_back( -N_3D-1+( N_3D*N_3D ) );
+                 directions.push_back( -N_3D+1+( N_3D*N_3D ) );
 
-  directions.push_back(-N_3D - 1 + (N_3D * N_3D));
-  directions.push_back(-N_3D + 1 + (N_3D * N_3D));
+                 directions.push_back( -N_3D-1+( 2*N_3D*N_3D ) );
+                 directions.push_back( -N_3D+1+( 2*N_3D*N_3D ) );
 
-  directions.push_back(-N_3D - 1 + (2 * N_3D * N_3D));
-  directions.push_back(-N_3D + 1 + (2 * N_3D * N_3D));
+                 directions.push_back( -1-( N_3D*N_3D ) );
+                 directions.push_back(  1-( N_3D*N_3D ) );
 
-  directions.push_back(-1 - (N_3D * N_3D));
-  directions.push_back(1 - (N_3D * N_3D));
+                 directions.push_back( -1-( 2*N_3D*N_3D ) );
+                 directions.push_back(  1-( 2*N_3D*N_3D ) );
 
-  directions.push_back(-1 - (2 * N_3D * N_3D));
-  directions.push_back(1 - (2 * N_3D * N_3D));
+                 directions.push_back( -N_3D-( N_3D*N_3D ) );
+                 directions.push_back(  N_3D-( N_3D*N_3D ) );
 
-  directions.push_back(-N_3D - (N_3D * N_3D));
-  directions.push_back(N_3D - (N_3D * N_3D));
+                 directions.push_back(  N_3D-1-( N_3D*N_3D ) );
+                 directions.push_back(  N_3D+1-( N_3D*N_3D ) );
 
-  directions.push_back(N_3D - 1 - (N_3D * N_3D));
-  directions.push_back(N_3D + 1 - (N_3D * N_3D));
+                 directions.push_back( -N_3D-1-( N_3D*N_3D ) );
+                 directions.push_back( -N_3D+1-( N_3D*N_3D ) );
 
-  directions.push_back(-N_3D - 1 - (N_3D * N_3D));
-  directions.push_back(-N_3D + 1 - (N_3D * N_3D));
+                 for(int i=0;i<directions.size();i++){
 
-  for (int i = 0; i < directions.size(); i++) {
+                     int index_neighbor = index+directions[ i ];
 
-    int index_neighbor = index + directions[i];
+                     std::vector<double> center = getCenterFromIndex( index_neighbor, N_3D, h_3D );
 
-    std::vector<double> center = getCenterFromIndex(index_neighbor, N_3D, h_3D);
+                     bool isInDomain = true;
 
-    bool isInDomain = true;
+                     for(int j=0;j<3;j++){
 
-    for (int j = 0; j < 3; j++) {
+                         if( center[ i ]<0.0 || center[ i ]>L_x ){
 
-      if (center[i] < 0.0 || center[i] > L_x) {
+                             isInDomain = false;
 
-        isInDomain = false;
+                             break;
+                             
+                         }
 
-        break;
-      }
-    }
+                     }
 
-    indicesNeighbors.push_back(index_neighbor);
-  }
+                     indicesNeighbors.push_back( index_neighbor );
 
-  return indicesNeighbors;
+                 }
+
+                 return indicesNeighbors;
+
 }
