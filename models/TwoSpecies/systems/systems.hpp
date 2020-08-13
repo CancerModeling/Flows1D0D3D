d87831e5476e861cd02902568f705ad37d992f0f
Prashant K. Jha <pjha.sci@gmail.com>
2020-08-13 11:27:16 -0500

    added two-species tumor model

---
+++ models/TwoSpecies/systems/systems.hpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2019 Prashant K. Jha
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TWOSP_SYSTEMS_H
+#define TWOSP_SYSTEMS_H
+
+// systems
+#include "tumor.hpp"
+#include "nutrient.hpp"
+
+
+#endif // TWOSP_SYSTEMS_H
