13745419d72b566d571cee8b869b411cf0d4c03b
Prashant K. Jha <pjha.sci@gmail.com>
2020-09-17 23:03:03 -0500

    add errors from all fields in nonlinear iteration

--- models/NetFVFE/systems/hypoxic.cpp
+++ models/NetFVFE/systems/hypoxic.cpp
@@ -148,7 +148,7 @@ void netfvfe::HypAssembly::assemble_1() {
         compute_rhs_mu =
             d_JxW[qp] * (deck.d_bar_E_phi_T * tum_old *
                          (4.0 * pow(tum_old, 2) - 6.0 * tum_old - 1.) +
-                         3. * deck.d_bar_E_phi_T * (pro_proj + nec_proj) -
+                         3. * deck.d_bar_E_phi_T * (pro_cur + nec_cur) -
                          deck.d_chi_c * nut_proj - deck.d_chi_h * ecm_proj);
 
         compute_mat_hyp =
