bf57da57f8e0f25b8e22c7eeedd828c173fafd8c
wagnandr <wagneran@ma.tum.de>
2021-02-02 19:51:58 +0100

    wip experiments with dg on simplified 1D mesh

---
+++ models/macrocirculation/systems/advection_assembly.hpp
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2021 Prashant K. Jha, Tobias Koeppl, Andreas Wagner
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TUMORMODELS_ADVECTION_ASSEMBLY_HPP
+#define TUMORMODELS_ADVECTION_ASSEMBLY_HPP
+
+namespace macrocirculation {
+
+class AdvectionAssembly{
+public:
+  AdvectionAssembly(){}
+
+private:
+
+};
+
+}
+
+#endif //TUMORMODELS_ADVECTION_ASSEMBLY_HPP
