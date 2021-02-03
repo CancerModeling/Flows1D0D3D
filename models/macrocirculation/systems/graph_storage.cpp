de469adebf03b515e6cef162cdf38c6b055d235e
wagnandr <wagneran@ma.tum.de>
2021-02-03 18:32:53 +0100

    moves GraphDataWriter into its own file

--- models/macrocirculation/systems/graph_storage.cpp
+++ models/macrocirculation/systems/graph_storage.cpp
@@ -32,7 +32,7 @@ bool Edge::is_pointing_to(std::size_t vertex_id) const {
   return p_neighbors[1] == vertex_id;
 }
 
-void GraphStorage::reorder_edges(Vertex & v) {
+void GraphStorage::reorder_edges(Vertex &v) {
   if (v.get_edge_neighbors().size() > 2)
     throw std::runtime_error("ordering edges on vertex with more than two neighbors.");
 
@@ -43,7 +43,7 @@ void GraphStorage::reorder_edges(Vertex & v) {
   const auto e0 = get_edge(v.get_edge_neighbors()[0]);
   const auto e1 = get_edge(v.get_edge_neighbors()[1]);
 
-  if ( !(e0->is_pointing_to(v.get_id()) != e1->is_pointing_to(v.get_id())) )
+  if (!(e0->is_pointing_to(v.get_id()) != e1->is_pointing_to(v.get_id())))
     throw std::runtime_error("edges cannot be reordered");
 
   // nothing to reorder
@@ -173,62 +173,6 @@ void GraphStorage::refine(std::size_t num_refinements) {
 
 double GraphStorage::num_edges() const {
   return p_edges.size();
-}
-
-void GraphDataWriter::add_midpoint_data(const std::string &name, std::vector<double> data)
-{
-  midpoint_data.push_back(std::move(NamedField(name, std::move(data))));
-}
-
-std::string GraphDataWriter::get_path(const std::string & filename, std::size_t time_idx) const
-{
-  std::string path = filename + "_";
-  path += std::to_string(time_idx);
-  path += ".vtk";
-  return path;
-}
-
-void GraphDataWriter::write_vtk(const std::string &filename, GraphStorage &storage, std::size_t time_idx) const
-{
-  const auto path = get_path(filename, time_idx);
-
-  const std::size_t number_of_points = storage.num_edges() * 2;
-  const std::size_t number_of_segments = storage.num_edges();
-
-  std::fstream filevtk;
-  filevtk.open(path, std::ios::out);
-  filevtk << "# vtk DataFile Version 2.0" << std::endl;
-  filevtk << "Network Nutrient Transport" << std::endl;
-  filevtk << "ASCII" << std::endl;
-  filevtk << "DATASET POLYDATA" << std::endl;
-
-  filevtk << "POINTS " << number_of_points << " float" << std::endl;
-  // the part for our point data
-  for (auto e_id : storage.get_edge_ids()) {
-    auto edge = storage.get_edge(e_id);
-    const auto &p0 = edge->get_coordinate_v0();
-    const auto &p1 = edge->get_coordinate_v1();
-
-    // write the two points into our file
-    filevtk << p0(0) << " " << p0(1) << " " << p0(2) << std::endl;
-    filevtk << p1(0) << " " << p1(1) << " " << p1(2) << std::endl;
-  }
-  filevtk << " " << std::endl;
-
-  filevtk << "LINES " << number_of_segments << " " << 3 * number_of_segments << std::endl;
-  for (std::size_t idx=0; idx< number_of_segments; idx+=1)
-    filevtk << "2 " << 2*idx << " " << 2*idx+1 << std::endl;
-  filevtk << " " << std::endl;
-
-  filevtk << "CELL_DATA " << number_of_segments << std::endl;
-  for (std::size_t idx = 0; idx < midpoint_data.size(); idx += 1) {
-    filevtk << "SCALARS " << midpoint_data[idx].name << " float 1" << std::endl;
-    filevtk << "LOOKUP_TABLE default" << std::endl;
-    for (auto e_id : storage.get_edge_ids()) {
-      auto edge = storage.get_edge(e_id);
-      filevtk << midpoint_data[idx].values[edge->get_id()] << std::endl;
-    }
-  }
 }
 
 
