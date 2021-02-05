34d661187c37451538cb663c0da439a6718aa04d
wagnandr <wagneran@ma.tum.de>
2021-02-05 13:38:43 +0100

    wip smaller fixes

--- models/macrocirculation/driver/main_nonlinear_flow.cpp
+++ models/macrocirculation/driver/main_nonlinear_flow.cpp
@@ -21,32 +21,34 @@ int main(int argc, char *argv[]) {
   // libmesh and then call the constructor of model
   lm::LibMeshInit init(argc, argv);
 
-  constexpr std::size_t degree = 3;
-
-  const double velocity = 2;
-  const double tau = 0.1;
-  const double t_end = 1;
-  const auto inflow_boundary_value = [](double t_now) -> double { return std::sin(M_PI * t_now); };
+  const double t_end = 1e-1;
+  const std::size_t max_iter = 3;
 
   // create the ascending aorta
   auto graph = std::make_shared<mc::GraphStorage>();
 
   std::size_t ascending_aorta_id = 1;
 
-  auto v0 = graph->create_vertex(lm::Point(0, 0, 0));
-  auto v1 = graph->create_vertex(lm::Point(4.0, 0, 0));
-  graph->connect(*v0, *v1, ascending_aorta_id);
-  graph->refine(5);
+//  auto v0 = graph->create_vertex(lm::Point(0, 0, 0));
+//  auto v1 = graph->create_vertex(lm::Point(4.0, 0, 0));
+//  graph->connect(*v0, *v1, ascending_aorta_id);
+//  graph->refine(5);
 
-  // const std::size_t N = 32;
-  // auto v_prev = graph.create_vertex(lm::Point(0, 0, 0));
-  // for (std::size_t k = 0; k < N; k += 1) {
-  //  auto v_next = graph.create_vertex(lm::Point((k + 1.) / N, 0, 0));
-  //  graph.connect(*v_prev, *v_next, ascending_aorta_id);
-  //  v_prev = v_next;
-  // }
+  const std::size_t N = 32;
+  auto v_prev = graph->create_vertex(lm::Point(0, 0, 0));
+  for (std::size_t k = 0; k < N; k += 1) {
+   auto v_next = graph->create_vertex(lm::Point(4*(k + 1.) / N, 0, 0));
+   graph->connect(*v_prev, *v_next, ascending_aorta_id);
+   v_prev = v_next;
+  }
 
   mc::ExplicitNonlinearFlowSolver solver(graph);
 
-  solver.solve();
+  for (std::size_t it = 0; it < max_iter; it+=1)
+  {
+    std::cout << "iter " << it << std::endl;
+    solver.solve();
+    if (solver.get_time() > t_end+1e-12)
+      break;
+  }
 }
--- models/macrocirculation/systems/explicit_nonlinear_flow_solver.cpp
+++ models/macrocirculation/systems/explicit_nonlinear_flow_solver.cpp
@@ -18,7 +18,7 @@ namespace macrocirculation {
 
 namespace lm = libMesh;
 
-constexpr std::size_t degree = 1;
+constexpr std::size_t degree = 0;
 
 void interpolate_constant(const GraphStorage & graph, const DofMapNetwork & dof_map, double value, std::size_t component, std::vector< double > & result)
 {
@@ -86,17 +86,22 @@ ExplicitNonlinearFlowSolver::ExplicitNonlinearFlowSolver(std::shared_ptr<GraphSt
 }
 
 void ExplicitNonlinearFlowSolver::solve() {
+  d_t_now += d_tau;
+  d_u_prev = d_u_now;
+
   calculate_fluxes();
   calculate_rhs();
   apply_inverse_mass();
 
-  std::cout << d_Q_up << std::endl;
-  std::cout << d_A_up << std::endl;
+  std::cout << "Q_up " << d_Q_up << std::endl;
+  std::cout << "A_up " << d_A_up << std::endl;
 
-  std::cout << d_rhs << std::endl;
-  std::cout << d_u_prev << std::endl;
-  std::cout << d_u_now << std::endl;
+  std::cout << "rhs " << d_rhs << std::endl;
+  std::cout << "u_prev " << d_u_prev << std::endl;
+  std::cout << "u_now " << d_u_now << std::endl;
 }
+
+double ExplicitNonlinearFlowSolver::get_time() const { return d_t_now; }
 
 void ExplicitNonlinearFlowSolver::calculate_fluxes() {
   // initial value of the flow
@@ -117,17 +122,17 @@ void ExplicitNonlinearFlowSolver::calculate_fluxes() {
   std::vector<std::size_t> A_dof_indices_r(num_basis_functions, 0);
 
   // local views of our previous solution
-  std::vector<double> Q_prev_loc_r(num_basis_functions, 0);
-  std::vector<double> A_prev_loc_r(num_basis_functions, 0);
   std::vector<double> Q_prev_loc_l(num_basis_functions, 0);
   std::vector<double> A_prev_loc_l(num_basis_functions, 0);
+  std::vector<double> Q_prev_loc_r(num_basis_functions, 0);
+  std::vector<double> A_prev_loc_r(num_basis_functions, 0);
 
   // previous solution evaluated at quadrature points
   // we have 2 quadrature points for the trapezoidal rule
-  std::vector<double> Q_prev_qp_r(2, 0);
-  std::vector<double> A_prev_qp_r(2, 0);
   std::vector<double> Q_prev_qp_l(2, 0);
   std::vector<double> A_prev_qp_l(2, 0);
+  std::vector<double> Q_prev_qp_r(2, 0);
+  std::vector<double> A_prev_qp_r(2, 0);
 
   for (const auto &v_id : d_graph->get_vertex_ids()) {
     const auto vertex = d_graph->get_vertex(v_id);
@@ -160,7 +165,7 @@ void ExplicitNonlinearFlowSolver::calculate_fluxes() {
         const double A = A_prev_qp_r[0];
 
         const double W1 = calculate_W1_value(Q, A, d_G0, d_rho, d_A0);
-        const double W2 = 2 * d_inflow_value_function(d_t_now) + calculate_W1_value(Q_init, A_init, d_G0, d_rho, d_A0);
+        const double W2 = 2 * d_inflow_value_function(d_t_now)/d_A0 + calculate_W1_value(Q_init, A_init, d_G0, d_rho, d_A0);
 
         double Q_up = 0, A_up = 0;
         solve_W12(Q_up, A_up, W1, W2, d_G0, d_rho, d_A0);
@@ -211,8 +216,8 @@ void ExplicitNonlinearFlowSolver::calculate_fluxes() {
 
       fe_l.evaluate_dof_at_quadrature_points(Q_prev_loc_l, Q_prev_qp_l);
       fe_l.evaluate_dof_at_quadrature_points(A_prev_loc_l, A_prev_qp_l);
-      fe_l.evaluate_dof_at_quadrature_points(Q_prev_loc_r, Q_prev_qp_r);
-      fe_l.evaluate_dof_at_quadrature_points(A_prev_loc_r, A_prev_qp_r);
+      fe_r.evaluate_dof_at_quadrature_points(Q_prev_loc_r, Q_prev_qp_r);
+      fe_r.evaluate_dof_at_quadrature_points(A_prev_loc_r, A_prev_qp_r);
 
       const double Q_l = Q_prev_qp_l[1];
       const double A_l = A_prev_qp_l[1];
@@ -224,6 +229,9 @@ void ExplicitNonlinearFlowSolver::calculate_fluxes() {
 
       double Q_up = 0, A_up = 0;
       solve_W12(Q_up, A_up, W1_r, W2_l, d_G0, d_rho, d_A0);
+
+      if (vertex->get_id() == 1)
+        std::cout << "halt" << std::endl;
 
       d_Q_up[vertex->get_id()] = Q_up;
       d_A_up[vertex->get_id()] = A_up;
@@ -314,12 +322,12 @@ void ExplicitNonlinearFlowSolver::calculate_rhs() {
         f_loc_A[i] += dphi[i][qp] * d_tau * F_A[qp] * JxW[qp];
       }
 
-      // boundary contributions
-      f_loc_Q[i] += d_tau * Q_up_1 * phi_b[i][1];
-      f_loc_Q[i] -= d_tau * Q_up_0 * phi_b[i][0];
+      // boundary contributions  - tau [ F(U_up) phi ] ds, keep attention to the minus!
+      f_loc_Q[i] -= d_tau * Q_up_1 * phi_b[i][1];
+      f_loc_Q[i] += d_tau * Q_up_0 * phi_b[i][0];
 
-      f_loc_A[i] += d_tau * F_A_eval(Q_up_1, A_up_1) * phi_b[i][1];
-      f_loc_A[i] -= d_tau * F_A_eval(Q_up_0, A_up_0) * phi_b[i][0];
+      f_loc_A[i] -= d_tau * F_A_eval(Q_up_1, A_up_1) * phi_b[i][1];
+      f_loc_A[i] += d_tau * F_A_eval(Q_up_0, A_up_0) * phi_b[i][0];
     }
 
     // copy into global vector
@@ -331,8 +339,10 @@ void ExplicitNonlinearFlowSolver::calculate_rhs() {
 }
 
 void ExplicitNonlinearFlowSolver::apply_inverse_mass() {
+  std::cout << "+ " << d_rhs << std::endl;
   for (std::size_t i=0; i<d_dof_map->num_dof(); i+=1)
     d_u_now[i] = d_inverse_mass[i] * d_rhs[i];
+  std::cout << "- " << d_u_now << std::endl;
 }
 
 } // namespace macrocirculation
