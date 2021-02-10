2df5cf700b6958e2cc2d3651163dfb2e8afc036c
wagnandr <wagneran@ma.tum.de>
2021-02-10 19:14:37 +0100

    adds degree to right hand side evaluator

--- models/macrocirculation/systems/explicit_nonlinear_flow_solver.cpp
+++ models/macrocirculation/systems/explicit_nonlinear_flow_solver.cpp
@@ -13,14 +13,11 @@
 #include "gmm.h"
 #include "graph_storage.hpp"
 #include "interpolate_to_vertices.hpp"
-#include "libmesh/libmesh.h"
-#include "vessel_formulas.hpp"
+#include "right_hand_side_evaluator.hpp"
 
 namespace macrocirculation {
 
 namespace lm = libMesh;
-
-constexpr std::size_t degree = 1;
 
 void interpolate_constant(const GraphStorage &graph, const DofMapNetwork &dof_map, double value, std::size_t component, std::vector<double> &result) {
   std::vector<std::size_t> dof_indices;
@@ -31,402 +28,36 @@ void interpolate_constant(const GraphStorage &graph, const DofMapNetwork &dof_ma
   }
 }
 
-void assemble_inverse_mass(const GraphStorage &graph, const DofMapNetwork &dof_map, std::vector<double> &inv_mass) {
-  // make sure that the inverse mass vector is large enough
-  assert(inv_mass.size() == dof_map.num_dof());
-
-  const std::size_t num_basis_functions = degree + 1;
-
-  // the squared norm of the legendre polynomials on [-1, +1]
-  std::vector<double> legendre_weight = {2, 2. / 3., 0.4, 0.285714};
-
-  // assert that we have precalculated enough weights
-  assert(num_basis_functions <= legendre_weight.size());
-
-  std::vector<std::size_t> Q_dof_indices(num_basis_functions, 0);
-  std::vector<std::size_t> A_dof_indices(num_basis_functions, 0);
-
-  for (const auto &e_id : graph.get_edge_ids()) {
-    const auto edge = graph.get_edge(e_id);
-
-    dof_map.dof_indices(*edge, Q_dof_indices, 0);
-    dof_map.dof_indices(*edge, A_dof_indices, 1);
-
-    const double edge_weight = edge->get_length() / 2;
-
-    for (std::size_t i = 0; i < num_basis_functions; i += 1) {
-      const double mass = legendre_weight[i] * edge_weight;
-      inv_mass[Q_dof_indices[i]] = 1. / mass;
-      inv_mass[A_dof_indices[i]] = 1. / mass;
-    }
-  }
-}
-
 ExplicitNonlinearFlowSolver::ExplicitNonlinearFlowSolver(std::shared_ptr<GraphStorage> graph)
     : d_graph(std::move(graph)),
       d_dof_map(std::make_shared<SimpleDofMapNetwork>(2, degree + 1, d_graph->num_edges())),
+      d_right_hand_side_evaluator(std::make_shared<RightHandSideEvaluator<degree>>(d_graph, d_dof_map)),
       d_tau(2.5e-4 / 4),
       d_t_now(0),
-      d_inflow_value_function(heart_beat_inflow),
       d_u_now(d_dof_map->num_dof()),
       d_u_prev(d_dof_map->num_dof()),
-      d_Q_up_el(d_graph->num_edges()),
-      d_Q_up_er(d_graph->num_edges()),
-      d_A_up_el(d_graph->num_edges()),
-      d_A_up_er(d_graph->num_edges()),
-      d_rhs(d_dof_map->num_dof()),
-      d_inverse_mass(d_dof_map->num_dof()),
-      d_G0(592.4e2), // 592.4 10^2 Pa,  TODO: Check if units are consistent!
-      d_rho(1.028),  // 1.028 kg/cm^3,  TODO: Check if units are consistent!
-      d_A0(6.97),    // 6.97 cm^2,      TODO: Check if units are consistent!
-      d_mu(4.5),     // 4.5 m Pa/s      TODO: Check if units are consistent!
-      d_gamma(2)     // Poiseuille flow
+      d_k0(d_dof_map->num_dof()),
+      d_A0(6.97) // 6.97 cm^2,      TODO: Check if units are consistent!
 {
   // set A constant to A0
   interpolate_constant(*d_graph, *d_dof_map, d_A0, 1, d_u_prev);
   interpolate_constant(*d_graph, *d_dof_map, d_A0, 1, d_u_now);
-  assemble_inverse_mass(*d_graph, *d_dof_map, d_inverse_mass);
 }
 
 void ExplicitNonlinearFlowSolver::solve() {
-  d_t_now += d_tau;
   d_u_prev = d_u_now;
+  d_t_now += d_tau;
+  const double t_prev = d_t_now - d_tau;
 
-  calculate_fluxes();
-  calculate_rhs();
-  apply_inverse_mass();
+  d_right_hand_side_evaluator->evaluate(t_prev, d_u_prev, d_k0);
 
-  /*
-  std::cout << "Q_up_er " << d_Q_up_er << std::endl;
-  std::cout << "Q_up_el " << d_Q_up_el << std::endl;
-  std::cout << "A_up_er " << d_A_up_er << std::endl;
-  std::cout << "A_up_el " << d_A_up_el << std::endl;
-
-  std::cout << "rhs " << d_rhs << std::endl;
-  std::cout << "u_prev " << d_u_prev << std::endl;
-  std::cout << "u_now " << d_u_now << std::endl;
-  */
+  // u_now = u_prev + tau * k0
+  gmm::add(d_u_prev, gmm::scaled(d_k0, d_tau), d_u_now);
 }
 
 double ExplicitNonlinearFlowSolver::get_time() const { return d_t_now; }
 
 void ExplicitNonlinearFlowSolver::set_tau(double tau) { d_tau = tau; }
-
-void ExplicitNonlinearFlowSolver::calculate_fluxes() {
-  // initial value of the flow
-  // TODO: make this more generic for other initial flow values
-  const double Q_init = 0;
-  const double A_init = d_A0;
-
-  const std::size_t num_basis_functions = degree + 1;
-
-  // finite-element for left and right edge
-  FETypeNetwork<degree> fe_l(create_trapezoidal_rule());
-  FETypeNetwork<degree> fe_r(create_trapezoidal_rule());
-  FETypeExteriorBdryNetwork<degree> fe_ext;
-
-  // dof indices for left and right edge
-  std::vector<std::size_t> Q_dof_indices_l(num_basis_functions, 0);
-  std::vector<std::size_t> A_dof_indices_l(num_basis_functions, 0);
-  std::vector<std::size_t> Q_dof_indices_r(num_basis_functions, 0);
-  std::vector<std::size_t> A_dof_indices_r(num_basis_functions, 0);
-
-  // local views of our previous solution
-  std::vector<double> Q_prev_loc_l(num_basis_functions, 0);
-  std::vector<double> A_prev_loc_l(num_basis_functions, 0);
-  std::vector<double> Q_prev_loc_r(num_basis_functions, 0);
-  std::vector<double> A_prev_loc_r(num_basis_functions, 0);
-
-  // previous solution evaluated at quadrature points
-  // we have 2 quadrature points for the trapezoidal rule
-  std::vector<double> Q_prev_qp_l(2, 0);
-  std::vector<double> A_prev_qp_l(2, 0);
-  std::vector<double> Q_prev_qp_r(2, 0);
-  std::vector<double> A_prev_qp_r(2, 0);
-
-  for (const auto &v_id : d_graph->get_vertex_ids()) {
-    const auto vertex = d_graph->get_vertex(v_id);
-
-    // exterior boundary
-    if (vertex->is_leaf()) {
-      // TODO: Make this more generic!
-      const bool is_inflow_boundary = (vertex->get_coordinate() - lm::Point(0, 0, 0)).norm() < 1e-10;
-
-      const auto edge_r = d_graph->get_edge(vertex->get_edge_neighbors()[0]);
-
-      fe_r.reinit(*edge_r);
-
-      d_dof_map->dof_indices(*edge_r, Q_dof_indices_r, 0);
-      d_dof_map->dof_indices(*edge_r, A_dof_indices_r, 1);
-
-      extract_dof(Q_dof_indices_r, d_u_prev, Q_prev_loc_r);
-      extract_dof(A_dof_indices_r, d_u_prev, A_prev_loc_r);
-
-      fe_r.evaluate_dof_at_quadrature_points(Q_prev_loc_r, Q_prev_qp_r);
-      fe_r.evaluate_dof_at_quadrature_points(A_prev_loc_r, A_prev_qp_r);
-
-      // inflow boundary
-      if (is_inflow_boundary) {
-        // we assert that the edge direction fits our assumptions
-        // TODO: Make this assumption more generic!
-        assert(edge_r->get_vertex_neighbors()[0] == vertex->get_id());
-
-        const double Q = Q_prev_qp_r[0];
-        const double A = A_prev_qp_r[0];
-
-        const double Q_star = d_inflow_value_function(d_t_now);
-        const double A_up = assemble_in_flow(Q, A, Q_star, d_G0, d_rho, d_A0);
-
-        d_Q_up_el[edge_r->get_id()] = Q_star;
-        d_A_up_el[edge_r->get_id()] = A_up;
-      }
-      // free outflow boundary
-      else {
-        // we assert that the edge direction fits our assumptions
-        // TODO: Make this assumption more generic!
-        assert(edge_r->get_vertex_neighbors()[1] == vertex->get_id());
-
-        const double Q = Q_prev_qp_r[1];
-        const double A = A_prev_qp_r[1];
-
-        const double W1 = calculate_W1_value(Q_init, A_init, d_G0, d_rho, d_A0);
-        const double W2 = calculate_W2_value(Q, A, d_G0, d_rho, d_A0);
-
-        double Q_up = 0, A_up = 0;
-        solve_W12(Q_up, A_up, W1, W2, d_G0, d_rho, d_A0);
-
-        d_Q_up_er[edge_r->get_id()] = Q_up;
-        d_A_up_er[edge_r->get_id()] = A_up;
-      }
-    }
-    // bifurcation boundary
-    else if (vertex->is_bifurcation()) {
-      if (vertex->get_edge_neighbors().size() > 3)
-        throw std::runtime_error("only 3 neighbors at bifurcation points possible.");
-
-      // get vertices
-      const auto e0 = d_graph->get_edge(vertex->get_edge_neighbors()[0]);
-      const auto e1 = d_graph->get_edge(vertex->get_edge_neighbors()[1]);
-      const auto e2 = d_graph->get_edge(vertex->get_edge_neighbors()[2]);
-
-      // check orientation
-      const bool e0_in = e0->is_pointing_to(vertex->get_id());
-      const bool e1_in = e1->is_pointing_to(vertex->get_id());
-      const bool e2_in = e2->is_pointing_to(vertex->get_id());
-
-      // evaluate on edge e0
-      d_dof_map->dof_indices(*e0, Q_dof_indices_l, 0);
-      d_dof_map->dof_indices(*e0, A_dof_indices_l, 1);
-      extract_dof(Q_dof_indices_l, d_u_prev, Q_prev_loc_l);
-      extract_dof(A_dof_indices_l, d_u_prev, A_prev_loc_l);
-      fe_ext.reinit(*vertex, *e0);
-      const double Q_e0 = fe_ext.evaluate_dof_at_boundary_points(Q_prev_loc_l);
-      const double A_e0 = fe_ext.evaluate_dof_at_boundary_points(A_prev_loc_l);
-
-      // evaluate on edge e1
-      d_dof_map->dof_indices(*e1, Q_dof_indices_l, 0);
-      d_dof_map->dof_indices(*e1, A_dof_indices_l, 1);
-      extract_dof(Q_dof_indices_l, d_u_prev, Q_prev_loc_l);
-      extract_dof(A_dof_indices_l, d_u_prev, A_prev_loc_l);
-      fe_ext.reinit(*vertex, *e1);
-      const double Q_e1 = fe_ext.evaluate_dof_at_boundary_points(Q_prev_loc_l);
-      const double A_e1 = fe_ext.evaluate_dof_at_boundary_points(A_prev_loc_l);
-
-      // evaluate on edge e2
-      d_dof_map->dof_indices(*e2, Q_dof_indices_l, 0);
-      d_dof_map->dof_indices(*e2, A_dof_indices_l, 1);
-      extract_dof(Q_dof_indices_l, d_u_prev, Q_prev_loc_l);
-      extract_dof(A_dof_indices_l, d_u_prev, A_prev_loc_l);
-      fe_ext.reinit(*vertex, *e2);
-      const double Q_e2 = fe_ext.evaluate_dof_at_boundary_points(Q_prev_loc_l);
-      const double A_e2 = fe_ext.evaluate_dof_at_boundary_points(A_prev_loc_l);
-
-      // get upwinded values at bifurcation
-      double Q_e0_up, A_e0_up;
-      double Q_e1_up, A_e1_up;
-      double Q_e2_up, A_e2_up;
-      const auto num_iter = solve_at_bifurcation(
-        Q_e0, A_e0, {d_G0, d_rho, d_A0}, e0_in,
-        Q_e1, A_e1, {d_G0, d_rho, d_A0}, e1_in,
-        Q_e2, A_e2, {d_G0, d_rho, d_A0}, e2_in,
-        Q_e0_up, A_e0_up,
-        Q_e1_up, A_e1_up,
-        Q_e2_up, A_e2_up);
-
-      // save upwinded values into upwind vector
-      if (e0_in) {
-        d_Q_up_er[e0->get_id()] = Q_e0_up;
-        d_A_up_er[e0->get_id()] = A_e0_up;
-      } else {
-        d_Q_up_el[e0->get_id()] = Q_e0_up;
-        d_A_up_el[e0->get_id()] = A_e0_up;
-      }
-
-      if (e1_in) {
-        d_Q_up_er[e1->get_id()] = Q_e1_up;
-        d_A_up_er[e1->get_id()] = A_e1_up;
-      } else {
-        d_Q_up_el[e1->get_id()] = Q_e1_up;
-        d_A_up_el[e1->get_id()] = A_e1_up;
-      }
-
-      if (e2_in) {
-        d_Q_up_er[e2->get_id()] = Q_e2_up;
-        d_A_up_er[e2->get_id()] = A_e2_up;
-      } else {
-        d_Q_up_el[e2->get_id()] = Q_e2_up;
-        d_A_up_el[e2->get_id()] = A_e2_up;
-      }
-    }
-    // inner boundary
-    else {
-      const auto edge_l = d_graph->get_edge(vertex->get_edge_neighbors()[0]);
-      const auto edge_r = d_graph->get_edge(vertex->get_edge_neighbors()[1]);
-
-      // we assert that the edge direction fits our assumptions
-      assert(edge_l->get_vertex_neighbors()[1] == vertex->get_id());
-      assert(edge_r->get_vertex_neighbors()[0] == vertex->get_id());
-
-      fe_l.reinit(*edge_l);
-      fe_r.reinit(*edge_r);
-
-      d_dof_map->dof_indices(*edge_l, Q_dof_indices_l, 0);
-      d_dof_map->dof_indices(*edge_l, A_dof_indices_l, 1);
-      d_dof_map->dof_indices(*edge_r, Q_dof_indices_r, 0);
-      d_dof_map->dof_indices(*edge_r, A_dof_indices_r, 1);
-
-      extract_dof(Q_dof_indices_l, d_u_prev, Q_prev_loc_l);
-      extract_dof(A_dof_indices_l, d_u_prev, A_prev_loc_l);
-      extract_dof(Q_dof_indices_r, d_u_prev, Q_prev_loc_r);
-      extract_dof(A_dof_indices_r, d_u_prev, A_prev_loc_r);
-
-      fe_l.evaluate_dof_at_quadrature_points(Q_prev_loc_l, Q_prev_qp_l);
-      fe_l.evaluate_dof_at_quadrature_points(A_prev_loc_l, A_prev_qp_l);
-      fe_r.evaluate_dof_at_quadrature_points(Q_prev_loc_r, Q_prev_qp_r);
-      fe_r.evaluate_dof_at_quadrature_points(A_prev_loc_r, A_prev_qp_r);
-
-      const double Q_l = Q_prev_qp_l[1];
-      const double A_l = A_prev_qp_l[1];
-      const double Q_r = Q_prev_qp_r[0];
-      const double A_r = A_prev_qp_r[0];
-
-      const double W2_l = calculate_W2_value(Q_l, A_l, d_G0, d_rho, d_A0);
-      const double W1_r = calculate_W1_value(Q_r, A_r, d_G0, d_rho, d_A0);
-
-      double Q_up = 0, A_up = 0;
-      solve_W12(Q_up, A_up, W1_r, W2_l, d_G0, d_rho, d_A0);
-
-      d_Q_up_er[edge_l->get_id()] = Q_up;
-      d_A_up_er[edge_l->get_id()] = A_up;
-      d_Q_up_el[edge_r->get_id()] = Q_up;
-      d_A_up_el[edge_r->get_id()] = A_up;
-    }
-  }
-}
-
-void ExplicitNonlinearFlowSolver::calculate_rhs() {
-  // first zero rhs
-  for (std::size_t idx = 0; idx < d_rhs.size(); idx += 1)
-    d_rhs[idx] = 0;
-
-  const std::size_t num_basis_functions = degree + 1;
-
-  std::vector<double> f_loc_Q(num_basis_functions);
-  std::vector<double> f_loc_A(num_basis_functions);
-
-  // data structures for cell contributions
-  FETypeNetwork<degree> fe(create_gauss4());
-  const auto &phi = fe.get_phi();
-  const auto &dphi = fe.get_dphi();
-  const auto &JxW = fe.get_JxW();
-
-  std::vector<std::size_t> Q_dof_indices(num_basis_functions, 0);
-  std::vector<std::size_t> A_dof_indices(num_basis_functions, 0);
-
-  std::vector<double> Q_prev_loc(num_basis_functions, 0);
-  std::vector<double> A_prev_loc(num_basis_functions, 0);
-
-  std::vector<double> Q_prev_qp(fe.num_quad_points(), 0);
-  std::vector<double> A_prev_qp(fe.num_quad_points(), 0);
-
-  // data structures for facet contributions
-  FETypeNetwork<degree> fe_boundary(create_trapezoidal_rule());
-  const auto &phi_b = fe_boundary.get_phi();
-
-  for (const auto &e_id : d_graph->get_edge_ids()) {
-    const auto edge = d_graph->get_edge(e_id);
-    fe.reinit(*edge);
-    fe_boundary.reinit(*edge);
-
-    // evaluate Q and A inside cell
-    d_dof_map->dof_indices(*edge, Q_dof_indices, 0);
-    d_dof_map->dof_indices(*edge, A_dof_indices, 1);
-
-    extract_dof(Q_dof_indices, d_u_prev, Q_prev_loc);
-    extract_dof(A_dof_indices, d_u_prev, A_prev_loc);
-
-    fe.evaluate_dof_at_quadrature_points(Q_prev_loc, Q_prev_qp);
-    fe.evaluate_dof_at_quadrature_points(A_prev_loc, A_prev_qp);
-
-    // evaluate Q and A on boundary
-    const double Q_up_0 = d_Q_up_el[edge->get_id()];
-    const double Q_up_1 = d_Q_up_er[edge->get_id()];
-    const double A_up_0 = d_A_up_el[edge->get_id()];
-    const double A_up_1 = d_A_up_er[edge->get_id()];
-
-    // the A-component of our F function
-    const auto F_Q_eval = [=](double Q, double A) -> double {
-      return std::pow(Q, 2) / A + d_G0 / (3 * d_rho * std::sqrt(d_A0)) * std::pow(A, 3. / 2.);
-    };
-
-    // evaluate F = (F_Q, F_A) at the quadrature points
-    const auto &F_A = Q_prev_qp;
-    std::vector<double> F_Q(Q_prev_qp.size(), 0);
-    for (std::size_t qp = 0; qp < fe.num_quad_points(); qp += 1)
-      F_Q[qp] = F_Q_eval(Q_prev_qp[qp], A_prev_qp[qp]);
-
-    // evaluate S = (S_Q, S_A) at the quadrature points
-    const double S_A = 0;
-    std::vector<double> S_Q(Q_prev_qp.size(), 0);
-    for (std::size_t qp = 0; qp < fe.num_quad_points(); qp += 1)
-      S_Q[qp] = -2 * d_mu * M_PI * (d_gamma + 2) * Q_prev_qp[qp] / A_prev_qp[qp];
-
-    for (std::size_t i = 0; i < num_basis_functions; i += 1) {
-      // rhs integral
-      f_loc_A[i] = 0;
-      f_loc_Q[i] = 0;
-
-      // cell contributions
-      for (std::size_t qp = 0; qp < fe.num_quad_points(); qp += 1) {
-        f_loc_Q[i] += phi[i][qp] * Q_prev_qp[qp] * JxW[qp];
-        f_loc_Q[i] += phi[i][qp] * d_tau * S_Q[qp] * JxW[qp];
-        f_loc_Q[i] += dphi[i][qp] * d_tau * F_Q[qp] * JxW[qp];
-
-        f_loc_A[i] += phi[i][qp] * A_prev_qp[qp] * JxW[qp];
-        f_loc_A[i] += phi[i][qp] * d_tau * S_A * JxW[qp];
-        f_loc_A[i] += dphi[i][qp] * d_tau * F_A[qp] * JxW[qp];
-      }
-
-      // boundary contributions  - tau [ F(U_up) phi ] ds, keep attention to the minus!
-      f_loc_Q[i] -= d_tau * F_Q_eval(Q_up_1, A_up_1) * phi_b[i][1];
-      f_loc_Q[i] += d_tau * F_Q_eval(Q_up_0, A_up_0) * phi_b[i][0];
-
-      f_loc_A[i] -= d_tau * Q_up_1 * phi_b[i][1];
-      f_loc_A[i] += d_tau * Q_up_0 * phi_b[i][0];
-    }
-
-    // copy into global vector
-    for (std::size_t i = 0; i < Q_dof_indices.size(); i += 1)
-      d_rhs[Q_dof_indices[i]] += f_loc_Q[i];
-    for (std::size_t i = 0; i < A_dof_indices.size(); i += 1)
-      d_rhs[A_dof_indices[i]] += f_loc_A[i];
-  }
-}
-
-void ExplicitNonlinearFlowSolver::apply_inverse_mass() {
-  for (std::size_t i = 0; i < d_dof_map->num_dof(); i += 1)
-    d_u_now[i] = d_inverse_mass[i] * d_rhs[i];
-}
 
 void ExplicitNonlinearFlowSolver::get_solution_on_vertices(std::vector<double> &Q_values, std::vector<double> &A_values) const {
   assert(Q_values.size() == d_graph->num_edges() * 2);
--- models/macrocirculation/systems/explicit_nonlinear_flow_solver.h
+++ models/macrocirculation/systems/explicit_nonlinear_flow_solver.h
@@ -11,18 +11,19 @@
 #include <cassert>
 #include <cmath>
 #include <functional>
+#include <gmm.h>
 #include <memory>
 #include <vector>
-#include <gmm.h>
 
 namespace macrocirculation {
+
+constexpr std::size_t degree = 1;
 
 // forward declarations
 class GraphStorage;
 class DofMapNetwork;
-
-/*! @brief Assembles the inverse mass. WARNING: Assumes legendre basis! */
-void assemble_inverse_mass(const GraphStorage &graph, const DofMapNetwork &dof_map, std::vector<double> &inv_mass);
+template<std::size_t degree>
+class RightHandSideEvaluator;
 
 /*! @brief Interpolates a constant value. WARNING: Assumes legendre basis! */
 void interpolate_constant(const GraphStorage &graph, const DofMapNetwork &dof_map, double value, std::size_t component, std::vector<double> &result);
@@ -43,8 +44,11 @@ private:
   /*! @brief The current domain for solving the equation. */
   std::shared_ptr<GraphStorage> d_graph;
 
-  /*! @brief The dof map for our domain */
+  /*! @brief The dof map for our domain. */
   std::shared_ptr<DofMapNetwork> d_dof_map;
+
+  /*! @brief Utility class for evaluating the right hand side, to allow different explicit schemes. */
+  std::shared_ptr<RightHandSideEvaluator<degree>> d_right_hand_side_evaluator;
 
   /*! @brief Current time step size. */
   double d_tau;
@@ -52,7 +56,8 @@ private:
   /*! @brief The current time. */
   double d_t_now;
 
-  std::function<double(double)> d_inflow_value_function;
+  // TODO: This value should be provided by the network.
+  double d_A0;
 
   /*! @brief The solution at the current time step. */
   std::vector<double> d_u_now;
@@ -60,47 +65,8 @@ private:
   /*! @brief The solution at the previous time step. */
   std::vector<double> d_u_prev;
 
-  /*! @brief The upwinded Q-flow from the left boundary of a macroedge,
-   *         ordered such that d_Q_up_el[edge_id] contains the respective flux.
-   * */
-  std::vector<double> d_Q_up_el;
-
-  /*! @brief The upwinded Q-flow from the right boundary of a macroedge,
-   *         ordered such that d_Q_up_er[edge_id] contains the respective flux.
-   * */
-  std::vector<double> d_Q_up_er;
-
-  /*! @brief The upwinded A-flow from the left boundary of a macroedge,
-   *         ordered such that d_A_up_el[edge_id] contains the respective flux.
-   * */
-  std::vector<double> d_A_up_el;
-
-  /*! @brief The upwinded A-flow from the right boundary of a macroedge,
-   *         ordered such that d_Q_up_er[edge_id] contains the respective flux.
-   * */
-  std::vector<double> d_A_up_er;
-
-  /*! @brief Our current rhs vector before multiplying it with the inverse mass. */
-  std::vector<double> d_rhs;
-
-  /*! @brief Our current inverse mass vector, defining the diagonal inverse mass matrix. */
-  std::vector<double> d_inverse_mass;
-
-  // All the parameters, which are now kept constant, but should change for vessel types.
-  double d_G0;
-  double d_rho;
-  double d_A0;
-  double d_mu;
-  double d_gamma;
-
-  /*! @brief Recalculates the current fluxes from the previous time step. */
-  void calculate_fluxes();
-
-  /*! @brief Assembles from the fluxes and the previous values a new right hand side function. */
-  void calculate_rhs();
-
-  /*! @brief Applies the inverse mass to our right hand side and saves the result to u_now. */
-  void apply_inverse_mass();
+  /*! @brief The solution at the previous time step. */
+  std::vector<double> d_k0;
 };
 
 } // namespace macrocirculation
