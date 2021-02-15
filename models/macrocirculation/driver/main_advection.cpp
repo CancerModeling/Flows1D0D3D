1244ad6de53b4a48a538f415ab232d3c0cd2a9ce
wagnandr <wagneran@ma.tum.de>
2021-02-15 18:20:00 +0100

    improves parameters advection solver

--- models/macrocirculation/driver/main_advection.cpp
+++ models/macrocirculation/driver/main_advection.cpp
@@ -16,12 +16,13 @@ namespace lm = libMesh;
 namespace mc = macrocirculation;
 
 int main(int argc, char *argv[]) {
-  constexpr std::size_t degree = 3;
+  constexpr std::size_t degree = 2;
 
-  const double velocity = 2;
-  const double tau = 0.1;
-  const double t_end = 1;
-  const auto inflow_boundary_value = [](double t_now) -> double { return std::sin(M_PI * t_now); };
+  const double velocity = 1;
+  const double tau = 0.001;
+  const double t_end = 2;
+  const auto inflow_boundary_value = [](double t_now) -> double { return std::sin(M_PI * 3 * t_now); };
+  // const auto inflow_boundary_value = [](double t_now) -> double { return 1; };
 
   // Note: This one requires pointer to comm and therefore we have to init
   // libmesh and then call the constructor of model
@@ -35,7 +36,7 @@ int main(int argc, char *argv[]) {
   auto v0 = graph->create_vertex(lm::Point(0, 0, 0));
   auto v1 = graph->create_vertex(lm::Point(1.0, 0, 0));
   graph->connect(*v0, *v1, ascending_aorta_id);
-  graph->refine(5);
+  graph->refine(10);
 
   // const std::size_t N = 32;
   // auto v_prev = graph.create_vertex(lm::Point(0, 0, 0));
@@ -46,6 +47,7 @@ int main(int argc, char *argv[]) {
   // }
 
   mc::AdvectionSolver<degree> solver(graph, tau, t_end, velocity, inflow_boundary_value);
+  solver.set_output_interval(100);
 
   solver.solve();
 }
