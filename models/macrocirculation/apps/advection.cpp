3cd8f53bb72508620166e3bba8d783b9f766f7cd
wagnandr <wagnandr@in.tum.de>
2021-03-14 20:12:18 +0100

    fixes advection example

--- models/macrocirculation/apps/advection.cpp
+++ models/macrocirculation/apps/advection.cpp
@@ -9,21 +9,21 @@
 #include "libmesh/libmesh.h"
 #include <cmath>
 
-#include "macrocirculation/advection_solver.hpp"
+#include "macrocirculation/implicit_advection_solver.hpp"
 #include "macrocirculation/graph_storage.hpp"
 
 namespace lm = libMesh;
 namespace mc = macrocirculation;
 
 int main(int argc, char *argv[]) {
-  /*
-  constexpr std::size_t degree = 0;
+  const std::size_t degree = 2;
+  const std::size_t num_micro_edges = 100;
 
   const double velocity = 1;
   const double tau = 0.001;
   const double t_end = 2;
-  //const auto inflow_boundary_value = [](double t_now) -> double { return std::sin(M_PI * 3 * t_now); };
-  const auto inflow_boundary_value = [](double t_now) -> double { return 1; };
+  const auto inflow_boundary_value = [](double t_now) -> double { return std::sin(M_PI * 3 * t_now); };
+  // const auto inflow_boundary_value = [](double t_now) -> double { return 1; };
 
   // Note: This one requires pointer to comm and therefore we have to init
   // libmesh and then call the constructor of model
@@ -34,10 +34,11 @@ int main(int argc, char *argv[]) {
 
   std::size_t ascending_aorta_id = 1;
 
-  auto v0 = graph->create_vertex(lm::Point(0, 0, 0));
-  auto v1 = graph->create_vertex(lm::Point(1.0, 0, 0));
-  graph->connect(*v0, *v1, ascending_aorta_id);
-  graph->refine(10);
+  auto v0 = graph->create_vertex();
+  auto v1 = graph->create_vertex();
+  auto edge = graph->connect(*v0, *v1, num_micro_edges);
+  edge->add_embedding_data({{ mc::Point(0, 0, 0), mc::Point(1, 0, 0) }});
+  edge->add_physical_data({ 0, 0, 0, 1. });
 
   // const std::size_t N = 32;
   // auto v_prev = graph.create_vertex(lm::Point(0, 0, 0));
@@ -47,9 +48,8 @@ int main(int argc, char *argv[]) {
   //  v_prev = v_next;
   // }
 
-  mc::AdvectionSolver<degree> solver(graph, tau, t_end, velocity, inflow_boundary_value);
+  mc::ImplicitAdvectionSolver solver(graph, degree, tau, t_end, velocity, inflow_boundary_value);
   solver.set_output_interval(100);
 
   solver.solve();
-   */
 }
--- models/macrocirculation/src/macrocirculation/implicit_advection_solver.cpp
+++ models/macrocirculation/src/macrocirculation/implicit_advection_solver.cpp
@@ -28,18 +28,22 @@ ImplicitAdvectionSolver::ImplicitAdvectionSolver(std::shared_ptr<GraphStorage> g
       d_graph(std::move(graph)) {}
 
 void ImplicitAdvectionSolver::solve() const {
+  // we only support one macro edge for now
+  assert(d_graph->num_edges() < 2);
+
   // assemble finite element system
   const std::size_t num_components = 1;
   const std::size_t num_basis_functions = d_degree + 1;
-  const std::size_t num_dofs = d_graph->num_edges() * num_components * num_basis_functions;
+
+  const auto dof_map = std::make_shared<DofMap>(d_graph->num_edges());
+  dof_map->create(MPI_COMM_WORLD, *d_graph, num_components, d_degree, true);
+
+  const std::size_t num_dofs = dof_map->num_dof();
 
   gmm::row_matrix<gmm::wsvector<double>> A(num_dofs, num_dofs);
   std::vector<double> f(num_dofs);
   std::vector<double> u_now(num_dofs, 0);
   std::vector<double> u_prev(num_dofs, 0);
-
-  const auto dof_map = std::make_shared<DofMap>(d_graph->num_edges());
-  dof_map->create(MPI_COMM_WORLD, *d_graph, num_components, d_degree, true);
 
   std::size_t it = 0;
 
@@ -125,7 +129,7 @@ void ImplicitAdvectionSolver::solve() const {
       }
     }
 
-    // assemble inner boundary integrals
+    // assemble boundary integrals
     {
       // functions evaluated on the inner cell boundaries
       FETypeNetwork fe(create_midpoint_rule(), d_degree);
@@ -141,15 +145,11 @@ void ImplicitAdvectionSolver::solve() const {
       gmm::row_matrix<gmm::wsvector<double>> A_rl_loc(num_components * num_basis_functions, num_components * num_basis_functions);
       gmm::row_matrix<gmm::wsvector<double>> A_rr_loc(num_components * num_basis_functions, num_components * num_basis_functions);
 
-      // block matrices for exterior boundaries
-      gmm::row_matrix<gmm::wsvector<double>> A_ext_loc(num_components * num_basis_functions, num_components * num_basis_functions);
 
-      // right hand side for inner boundaries
-      std::vector<double> f_ext_loc(num_components * num_basis_functions);
+      std::vector<std::size_t> dof_indices_l(num_basis_functions);
+      std::vector<std::size_t> dof_indices_r(num_basis_functions);
 
-      std::vector<std::size_t> dof_indices_l;
-      std::vector<std::size_t> dof_indices_r;
-
+      // assemble inner boundary integrals
       for (const auto &e_id : d_graph->get_edge_ids()) {
         const auto macro_edge = d_graph->get_edge(e_id);
 
@@ -211,26 +211,43 @@ void ImplicitAdvectionSolver::solve() const {
             }
           }
         }
-
-        for (const auto &edge : macro_edge->micro_edges()) {
-        }
       }
     }
 
     // assemble outer boundaries
     {
-    }
+      // functions evaluated on the inner cell boundaries
+      FETypeNetwork fe(create_midpoint_rule(), d_degree);
+
+      const auto &phi_l = fe.get_phi_boundary()[0];
+      const auto &phi_r = fe.get_phi_boundary()[1];
 
 
-    /*
-      for (const auto &v_id : d_graph->get_vertex_ids()) {
-        const auto vertex = d_graph->get_vertex(v_id);
+      // block matrices for exterior boundaries
+      gmm::row_matrix<gmm::wsvector<double>> A_ext_loc(num_components * num_basis_functions, num_components * num_basis_functions);
 
-        // exterior boundary
-        if (vertex->is_leaf()) {
-          const auto edge = d_graph->get_edge(vertex->get_edge_neighbors()[0]);
-          dof_map->dof_indices(*edge, dof_indices);
-          fe_ext.reinit(*vertex, *edge);
+      // right hand side for inner boundaries
+      std::vector<double> f_ext_loc(num_components * num_basis_functions);
+
+      std::vector<std::size_t > dof_indices(num_components * num_basis_functions);
+
+      for (const auto &e_id : d_graph->get_edge_ids()) {
+        const auto macro_edge = d_graph->get_edge(e_id);
+
+        const auto &local_dof_map = dof_map->get_local_dof_map(*macro_edge);
+        const auto &param = macro_edge->get_physical_data();
+        const double h = param.length / local_dof_map.num_micro_edges();
+
+        fe.reinit(h);
+
+        std::array< MicroVertex, 2 > micro_vertices = {macro_edge->left_micro_vertex(), macro_edge->right_micro_vertex()};
+        std::array< MicroEdge, 2 > neighbor_edges = {*micro_vertices[0].get_right_edge(), *micro_vertices[1].get_left_edge()};
+        std::array< double, 2 > normal = {-1, +1};
+        std::array< std::reference_wrapper<const std::vector< double >>, 2 > phi = {phi_l, phi_r};
+
+        for (std::size_t v_idx=0; v_idx < 2; v_idx += 1)
+        {
+          local_dof_map.dof_indices(neighbor_edges[v_idx], 0, dof_indices);
 
           // zero local system
           for (std::size_t i = 0; i < num_basis_functions; i += 1) {
@@ -240,17 +257,17 @@ void ImplicitAdvectionSolver::solve() const {
           }
 
           // inflow boundary
-          if (fe_ext.get_normal() * d_velocity < 0) {
+          if (normal[v_idx] * d_velocity < 0) {
             const auto inflow_value = d_inflow_value_fct(t_now);
             for (unsigned int i = 0; i < num_basis_functions; i += 1) {
-              f_ext_loc[i] += (-d_tau) * inflow_value * d_velocity * fe_ext.get_normal() * phi[i];
+              f_ext_loc[i] += (-d_tau) * inflow_value * d_velocity * normal[v_idx] * phi[v_idx].get()[i];
             }
           }
-          // outflow boundary
+            // outflow boundary
           else {
             for (unsigned int i = 0; i < num_basis_functions; i += 1) {
               for (unsigned int j = 0; j < num_basis_functions; j += 1) {
-                A_ext_loc(i, j) += d_tau * phi[i] * d_velocity * fe_ext.get_normal() * phi[j];
+                A_ext_loc(i, j) += d_tau * phi[v_idx].get()[i] * d_velocity * normal[v_idx] * phi[v_idx].get()[j];
               }
             }
           }
@@ -263,94 +280,24 @@ void ImplicitAdvectionSolver::solve() const {
             }
           }
         }
-        // inner boundary
-        else {
-          const auto edge_l = d_graph->get_edge(vertex->get_edge_neighbors()[0]);
-          const auto edge_r = d_graph->get_edge(vertex->get_edge_neighbors()[1]);
-          dof_map->dof_indices(*edge_l, dof_indices_l);
-          dof_map->dof_indices(*edge_r, dof_indices_r);
-          fe_inner.reinit(*vertex, *edge_l, *edge_r);
-
-          // zero local system
-          for (std::size_t i = 0; i < num_basis_functions; i += 1) {
-            for (std::size_t j = 0; j < num_basis_functions; j += 1) {
-              A_ll_loc(i, j) = 0;
-              A_lr_loc(i, j) = 0;
-              A_rl_loc(i, j) = 0;
-              A_rr_loc(i, j) = 0;
-            }
-          }
-
-          // ll
-          if (d_velocity * 1 > 0) {
-            for (std::size_t i = 0; i < num_basis_functions; i += 1)
-              for (std::size_t j = 0; j < num_basis_functions; j += 1)
-                A_ll_loc(i, j) += d_tau * phi_l[j] * d_velocity * phi_l[i];
-          }
-
-          // lr
-          if (d_velocity * 1 < 0) {
-            for (std::size_t i = 0; i < num_basis_functions; i += 1)
-              for (std::size_t j = 0; j < num_basis_functions; j += 1)
-                A_lr_loc(i, j) += d_tau * phi_r[j] * d_velocity * phi_l[i];
-          }
-
-          // rl
-          if (d_velocity * 1 > 0) {
-            for (std::size_t i = 0; i < num_basis_functions; i += 1)
-              for (std::size_t j = 0; j < num_basis_functions; j += 1)
-                A_rl_loc(i, j) += (-d_tau) * phi_l[j] * d_velocity * phi_r[i];
-          }
-
-          // rr
-          if (d_velocity * 1 < 0) {
-            for (std::size_t i = 0; i < num_basis_functions; i += 1)
-              for (std::size_t j = 0; j < num_basis_functions; j += 1)
-                A_rr_loc(i, j) += (-d_tau) * phi_r[j] * d_velocity * phi_r[i];
-          }
-
-          // copy into global matrix
-          for (std::size_t i = 0; i < dof_indices.size(); i += 1) {
-            for (std::size_t j = 0; j < dof_indices.size(); j += 1) {
-              A(dof_indices_r[i], dof_indices_r[j]) += A_rr_loc(i, j);
-              A(dof_indices_l[i], dof_indices_l[j]) += A_ll_loc(i, j);
-              A(dof_indices_r[i], dof_indices_l[j]) += A_rl_loc(i, j);
-              A(dof_indices_l[i], dof_indices_r[j]) += A_lr_loc(i, j);
-            }
-          }
-        }
       }
     }
-       */
 
-    /*
     gmm::iteration iter(1.0E-10);
     gmm::ilu_precond<gmm::row_matrix<gmm::wsvector<double>>> PR(A);
     gmm::gmres(A, u_now, f, PR, 500, iter);
 
-
     if (it % d_output_interval == 0) {
       std::cout << "it = " << it << std::endl;
-      FETypeNetwork<DEGREE> fe(create_trapezoidal_rule());
 
-      std::vector<double> u_vertex(d_graph->num_edges() * 2, 0);
-      std::vector<Point> points(d_graph->num_edges() * 2);
-      interpolate_to_vertices<DEGREE>(d_comm, *d_graph, *dof_map, 0, u_now, points, u_vertex);
-
-      std::vector<double> u_mid(d_graph->num_edges(), 0);
-      {
-        for (std::size_t idx = 0; idx < d_graph->num_edges(); idx += 1) {
-          u_mid[idx] = u_now[idx * num_basis_functions];
-          if (num_basis_functions > 2)
-            u_mid[idx] += -0.5 * u_now[idx * num_basis_functions + 2];
-        }
-      }
+      std::vector<Point> points;
+      std::vector<double> u_vertex_values;
+      interpolate_to_vertices(MPI_COMM_WORLD, *d_graph, *dof_map, 0, u_now, points, u_vertex_values);
 
       writer.set_points(points);
-      writer.add_vertex_data("concentration", u_vertex);
+      writer.add_vertex_data("concentration", u_vertex_values);
       writer.write(t_now);
     }
-     */
   }
 }
 
