e7fa19656037279b2507c2fee71b64b58f695ca1
wagnandr <wagnandr@in.tum.de>
2021-04-18 21:09:17 +0200

    wip transport

--- models/macrocirculation/src/macrocirculation/Transport.h
+++ models/macrocirculation/src/macrocirculation/Transport.h
@@ -10,26 +10,28 @@
 
 #include <cmath>
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "communication/mpi.hpp"
 #include "dof_map.hpp"
 #include "fe_type.hpp"
+#include "flow_upwind_evaluator.hpp"
 #include "gmm.h"
 #include "graph_storage.hpp"
 #include "right_hand_side_evaluator.hpp"
 
 namespace macrocirculation {
 
-
-/*
 class Transport {
 public:
   Transport(MPI_Comm comm, std::shared_ptr<GraphStorage> graph, std::shared_ptr<DofMap> dof_map_flow, std::shared_ptr<DofMap> dof_map_transport)
       : d_comm(comm),
-        d_graph(d_graph),
-        d_dof_map_flow(d_dof_map_flow),
-        d_dof_map_transport(d_dof_map_transport) {}
+        d_graph(std::move(graph)),
+        d_dof_map_flow(std::move(dof_map_flow)),
+        d_dof_map_transport(std::move(dof_map_transport)),
+        d_flow_upwind_evaluator(comm, d_graph, d_dof_map_flow),
+        d_edge_boundary_communicator(Communicator::create_edge_boundary_value_communicator(comm, d_graph)) {}
 
   void evaluate_macro_edge_boundary_values(const std::vector<double> &u_prev, const std::vector<double> &gamma_prev) {
     std::vector<std::size_t> dof_indices(4, 0);
@@ -39,67 +41,41 @@ public:
       const auto edge = d_graph->get_edge(e_id);
       const auto &param = edge->get_physical_data();
 
-      {
-        const auto &local_dof_map = d_dof_map_flow->get_local_dof_map(*edge);
-        const double h = param.length / double(local_dof_map.num_micro_edges());
+      const auto &local_dof_map = d_dof_map_transport->get_local_dof_map(*edge);
+      const double h = param.length / double(local_dof_map.num_micro_edges());
 
-        FETypeNetwork fe(create_midpoint_rule(), local_dof_map.num_basis_functions() - 1);
-        fe.reinit(h);
+      FETypeNetwork fe(create_midpoint_rule(), local_dof_map.num_basis_functions() - 1);
+      fe.reinit(h);
 
-        dof_indices.resize(local_dof_map.num_basis_functions());
-        local_dofs.resize(local_dof_map.num_basis_functions());
+      dof_indices.resize(local_dof_map.num_basis_functions());
+      local_dofs.resize(local_dof_map.num_basis_functions());
 
-        local_dof_map.dof_indices(0, 0, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_Q_macro_edge_boundary_value_l[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).left;
+      local_dof_map.dof_indices(0, 0, dof_indices);
+      extract_dof(dof_indices, u_prev, local_dofs);
+      d_gamma_macro_edge_boundary_value[2 * edge->get_id() + 0] = fe.evaluate_dof_at_boundary_points(local_dofs).left;
 
-        local_dof_map.dof_indices(0, 1, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_A_macro_edge_boundary_value_l[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).left;
-
-        local_dof_map.dof_indices(local_dof_map.num_micro_edges() - 1, 0, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_Q_macro_edge_boundary_value_r[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).right;
-
-        local_dof_map.dof_indices(local_dof_map.num_micro_edges() - 1, 1, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_A_macro_edge_boundary_value_r[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).right;
-      }
-
-      {
-        const auto &local_dof_map = d_dof_map_transport->get_local_dof_map(*edge);
-        const double h = param.length / double(local_dof_map.num_micro_edges());
-
-        FETypeNetwork fe(create_midpoint_rule(), local_dof_map.num_basis_functions() - 1);
-        fe.reinit(h);
-
-        dof_indices.resize(local_dof_map.num_basis_functions());
-        local_dofs.resize(local_dof_map.num_basis_functions());
-
-        local_dof_map.dof_indices(0, 0, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_gamma_macro_edge_boundary_value_l[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).left;
-
-        local_dof_map.dof_indices(local_dof_map.num_micro_edges() - 1, 0, dof_indices);
-        extract_dof(dof_indices, u_prev, local_dofs);
-        d_gamma_macro_edge_boundary_value_r[edge->get_id()] = fe.evaluate_dof_at_boundary_points(local_dofs).right;
-      }
+      local_dof_map.dof_indices(local_dof_map.num_micro_edges() - 1, 0, dof_indices);
+      extract_dof(dof_indices, u_prev, local_dofs);
+      d_gamma_macro_edge_boundary_value[2 * edge->get_id() + 1] = fe.evaluate_dof_at_boundary_points(local_dofs).right;
     }
   }
 
   std::vector<double> &get_solution() { return d_solution; }
 
-  void solve(double t, double dt, const std::vector<double> &u_old) {
-    evaluate_macro_edge_boundary_values(u_old, d_solution);
-    // TODO: communication with mpi
-    calculate_fluxes_at_nfurcations(t);
-    assemble_rhs(u_old, d_rhs);
+  void solve(double t, double dt, const std::vector<double> &u_prev) {
+    evaluate_macro_edge_boundary_values(u_prev, d_solution);
+    d_edge_boundary_communicator.update_ghost_layer(d_gamma_macro_edge_boundary_value);
+
+    calculate_fluxes_at_nfurcations(t, u_prev);
+    assemble_rhs(u_prev, d_rhs);
     apply_inverse_mass();
   }
 
-  void calculate_fluxes_on_macro_edge(const Edge &edge,
+  void calculate_fluxes_on_macro_edge(const double t,
+                                      const Edge &edge,
                                       const std::vector<double> &u_prev,
                                       std::vector<double> &gamma_fluxes_edge) {
+
     const auto local_dof_map_flow = d_dof_map_flow->get_local_dof_map(edge);
     const auto local_dof_map_transport = d_dof_map_transport->get_local_dof_map(edge);
 
@@ -111,19 +87,7 @@ public:
     std::vector<double> Q_up_macro_edge(local_dof_map_flow.num_micro_vertices(), 0);
     std::vector<double> A_up_macro_edge(local_dof_map_flow.num_micro_vertices(), 0);
 
-    calculate_inner_fluxes_on_macro_edge(*d_dof_map_flow, edge, u_prev, Q_up_macro_edge, A_up_macro_edge);
-
-    // update left fluxes
-    Q_up_macro_edge[0] = d_Q_macro_edge_flux_l[edge.get_id()];
-    A_up_macro_edge[0] = d_A_macro_edge_flux_l[edge.get_id()];
-
-    // update right fluxes
-    Q_up_macro_edge[local_dof_map.num_micro_vertices() - 1] = d_Q_macro_edge_flux_r[edge.get_id()];
-    A_up_macro_edge[local_dof_map.num_micro_vertices() - 1] = d_A_macro_edge_flux_r[edge.get_id()];
-
-    cal
-
-
+    d_flow_upwind_evaluator.get_fluxes_on_macro_edge(t, edge, u_prev, Q_up_macro_edge, A_up_macro_edge);
 
     const auto &param = edge.get_physical_data();
     const double h = param.length / double(local_dof_map_transport.num_micro_edges());
@@ -146,22 +110,6 @@ public:
       const std::size_t local_micro_edge_id_l = micro_vertex_id - 1;
       const std::size_t local_micro_edge_id_r = micro_vertex_id;
 
-      // get Q:
-      local_dof_map_flow.dof_indices(local_micro_edge_id_l, 0, dof_indices);
-      extract_dof(dof_indices, u_prev, extracted_dof);
-      const double Q_l = fe.evaluate_dof_at_boundary_points(extracted_dof).right;
-      local_dof_map_flow.dof_indices(local_micro_edge_id_r, 0, dof_indices);
-      extract_dof(dof_indices, u_prev, extracted_dof);
-      const double Q_r = fe.evaluate_dof_at_boundary_points(extracted_dof).left;
-
-      // get A:
-      local_dof_map_flow.dof_indices(local_micro_edge_id_l, 1, dof_indices);
-      extract_dof(dof_indices, u_prev, extracted_dof);
-      const double A_l = fe.evaluate_dof_at_boundary_points(extracted_dof).right;
-      local_dof_map_flow.dof_indices(local_micro_edge_id_r, 1, dof_indices);
-      extract_dof(dof_indices, u_prev, extracted_dof);
-      const double A_r = fe.evaluate_dof_at_boundary_points(extracted_dof).left;
-
       // get gamma:
       local_dof_map_transport.dof_indices(local_micro_edge_id_l, 0, dof_indices);
       extract_dof(dof_indices, u_prev, extracted_dof);
@@ -170,55 +118,20 @@ public:
       extract_dof(dof_indices, u_prev, extracted_dof);
       const double gamma_r = fe.evaluate_dof_at_boundary_points(extracted_dof).left;
 
-      // TODO: what if Q_l/A_l < 0 but also Q_r/A_r < 0?
-      if (Q_l/A_l < 0)
+      const double v = Q_up_macro_edge[micro_vertex_id] / A_up_macro_edge[micro_vertex_id];
+      const double gamma = (v < 0) ? gamma_r : gamma_l;
 
-
-
-      local_dof_map_flow.dof_indices(extracted_dof, 0, dof_indices);
-
-      local_dof_map.dof_indices(local_micro_edge_id_l, 0, Q_dof_indices_l);
-      local_dof_map.dof_indices(local_micro_edge_id_r, 0, Q_dof_indices_r);
-      local_dof_map.dof_indices(local_micro_edge_id_l, 1, A_dof_indices_l);
-      local_dof_map.dof_indices(local_micro_edge_id_r, 1, A_dof_indices_r);
-
-      extract_dof(Q_dof_indices_l, u_prev, Q_prev_loc_l);
-      extract_dof(A_dof_indices_l, u_prev, A_prev_loc_l);
-      extract_dof(Q_dof_indices_r, u_prev, Q_prev_loc_r);
-      extract_dof(A_dof_indices_r, u_prev, A_prev_loc_r);
-
-      fe.evaluate_dof_at_quadrature_points(Q_prev_loc_l, Q_prev_qp_l);
-      fe.evaluate_dof_at_quadrature_points(A_prev_loc_l, A_prev_qp_l);
-      fe.evaluate_dof_at_quadrature_points(Q_prev_loc_r, Q_prev_qp_r);
-      fe.evaluate_dof_at_quadrature_points(A_prev_loc_r, A_prev_qp_r);
-
-      const double Q_l = Q_prev_qp_l[1];
-      const double A_l = A_prev_qp_l[1];
-      const double Q_r = Q_prev_qp_r[0];
-      const double A_r = A_prev_qp_r[0];
-
-      const double W2_l = calculate_W2_value(Q_l, A_l, param.G0, param.rho, param.A0);
-      const double W1_r = calculate_W1_value(Q_r, A_r, param.G0, param.rho, param.A0);
-
-      double Q_up = 0, A_up = 0;
-
-      solve_W12(Q_up, A_up, W1_r, W2_l, param.G0, param.rho, param.A0);
-
-      Q_up_macro_edge[micro_vertex_id] = Q_up;
-      A_up_macro_edge[micro_vertex_id] = A_up;
+      gamma_fluxes_edge[micro_vertex_id] = gamma * v;
     }
 
     // update left fluxes
-    Q_up_macro_edge[0] = d_Q_macro_edge_flux_l[edge.get_id()];
-    A_up_macro_edge[0] = d_A_macro_edge_flux_l[edge.get_id()];
+    gamma_fluxes_edge[0] = d_gamma_flux_l[edge.get_id()];
 
     // update right fluxes
-    Q_up_macro_edge[local_dof_map.num_micro_vertices() - 1] = d_Q_macro_edge_flux_r[edge.get_id()];
-    A_up_macro_edge[local_dof_map.num_micro_vertices() - 1] = d_A_macro_edge_flux_r[edge.get_id()];
+    gamma_fluxes_edge[local_dof_map_transport.num_micro_vertices() - 1] = d_gamma_flux_r[edge.get_id()];
   }
 
-
-  void assemble_rhs(const std::vector<double> &u_prev, std::vector<double> &rhs) {
+  void assemble_rhs(double t, const std::vector<double> &u_prev, std::vector<double> &rhs) {
     // first zero rhs
     for (std::size_t idx = 0; idx < rhs.size(); idx += 1)
       rhs[idx] = 0;
@@ -233,15 +146,15 @@ public:
 
       // calculate fluxes on macro edge
       gamma_flux_macro_edge.resize(local_dof_map_transport.num_micro_vertices());
-      calculate_fluxes_on_macro_edge(*edge, u_prev, gamma_flux_macro_edge);
+      calculate_fluxes_on_macro_edge(t, *edge, u_prev, gamma_flux_macro_edge);
 
-      const std::size_t num_basis_functions = local_dof_map.num_basis_functions();
+      const std::size_t num_basis_functions = local_dof_map_transport.num_basis_functions();
 
       std::vector<double> f_loc_Q(num_basis_functions);
       std::vector<double> f_loc_A(num_basis_functions);
 
       QuadratureFormula qf = create_gauss4();
-      FETypeNetwork fe(qf, local_dof_map.num_basis_functions() - 1);
+      FETypeNetwork fe(qf, local_dof_map_transport.num_basis_functions() - 1);
 
       const auto &phi = fe.get_phi();
       const auto &phi_b = fe.get_phi_boundary();
@@ -337,17 +250,22 @@ public:
   }
 
 private:
-  void calculate_fluxes_at_nfurcations(double t) {
+  void calculate_fluxes_at_nfurcations(double t, const std::vector<double> &u_prev) {
+    std::vector<double> Q_up_values(0, 0);
+    std::vector<double> A_up_values(0, 0);
+
     for (auto &v_id : d_graph->get_vertex_ids()) {
       auto &vertex = *d_graph->get_vertex(v_id);
+
+      d_flow_upwind_evaluator.get_fluxes_on_nfurcation(t, vertex, Q_up_values, A_up_values);
 
       if (vertex.is_leaf()) {
         auto &edge = *d_graph->get_edge(vertex.get_edge_neighbors()[0]);
 
-        double Q = edge.is_pointing_to(vertex.get_id()) ? d_Q_macro_edge_boundary_value_r[edge.get_id()] : d_Q_macro_edge_boundary_value_l[edge.get_id()];
-        double A = edge.is_pointing_to(vertex.get_id()) ? d_A_macro_edge_boundary_value_r[edge.get_id()] : d_A_macro_edge_boundary_value_l[edge.get_id()];
+        const double Q = Q_up_values.front();
+        const double A = A_up_values.front();
 
-        double v = Q / A;
+        const double v = Q / A;
 
         const bool is_inflow = (v > 1e-8 && !edge.is_pointing_to(vertex.get_id())) || (v < 1e-8 && edge.is_pointing_to(vertex.get_id()));
 
@@ -370,58 +288,49 @@ private:
         } else {
           // outflow:
           if (!edge.is_pointing_to(vertex.get_id())) {
-            d_gamma_flux_l[edge.get_id()] = v * d_gamma_macro_edge_boundary_value_l[edge.get_id()];
+            d_gamma_flux_l[edge.get_id()] = v * d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 0];
           } else {
-            d_gamma_flux_r[edge.get_id()] = v * d_gamma_macro_edge_boundary_value_r[edge.get_id()];
+            d_gamma_flux_r[edge.get_id()] = v * d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 1];
           }
         }
       } else if (vertex.is_bifurcation()) {
-        std::vector<double> Q;
-        std::vector<double> A;
+        // do nothing if there is no flow
+        if (gmm::vect_norminf(Q_up_values) < 1e-8)
+          continue;
+
         std::vector<double> gamma;
         std::vector<bool> is_in;
 
         double Q_out = 0;
         double N_in = 0;
 
-        for (auto e_id : vertex.get_edge_neighbors()) {
-          auto &edge = *d_graph->get_edge(e_id);
-          double Q_value = edge.is_pointing_to(vertex.get_id()) ? d_Q_macro_edge_boundary_value_r[edge.get_id()] : d_Q_macro_edge_boundary_value_l[edge.get_id()];
-          double A_value = edge.is_pointing_to(vertex.get_id()) ? d_A_macro_edge_boundary_value_r[edge.get_id()] : d_A_macro_edge_boundary_value_l[edge.get_id()];
-          double gamma_value = edge.is_pointing_to(vertex.get_id()) ? d_gamma_macro_edge_boundary_value_r[edge.get_id()] : d_gamma_macro_edge_boundary_value_l[edge.get_id()];
-          double v = Q_value / A_value;
+        for (size_t k = 0; k < vertex.get_edge_neighbors().size(); k += 1) {
+          auto &edge = *d_graph->get_edge(vertex.get_edge_neighbors()[k]);
+          double gamma_value = edge.is_pointing_to(vertex.get_id()) ? d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 1] : d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 0];
+          double v = Q_up_values[k] / A_up_values[k];
           const bool is_inflow_value = (v > 1e-8 && !edge.is_pointing_to(vertex.get_id())) || (v < 1e-8 && edge.is_pointing_to(vertex.get_id()));
 
-          Q.push_back(Q_value);
-          A.push_back(A_value);
           gamma.push_back(gamma_value);
           is_in.push_back((is_inflow_value));
 
           if (!is_inflow_value)
-            Q_out += std::abs(Q_value);
+            Q_out += std::abs(Q_up_values[k]);
 
           if (is_inflow_value)
-            N_in += std::abs(Q_value) / A_value * gamma_value;
+            N_in += std::abs(Q_up_values[k]) / A_up_values[k] * gamma_value;
         }
-
-        // do nothing if there is no flow
-        if (gmm::vect_norminf(Q) < 1e-8)
-          continue;
-
-        gmm::row_matrix<gmm::wsvector<double>> mat(vertex.get_edge_neighbors().size(), vertex.get_edge_neighbors().size());
-        std::vector<double> rhs(vertex.get_edge_neighbors().size());
 
         for (std::size_t i = 0; i < vertex.get_edge_neighbors().size(); i += 1) {
           auto &edge = *d_graph->get_edge(vertex.get_edge_neighbors()[i]);
-          double v = Q[edge.get_id()] / A[edge.get_id()];
+          double v = Q_up_values[i] / A_up_values[i];
 
           if (is_in[i]) {
             if (edge.is_pointing_to(v_id))
-              d_gamma_flux_r[edge.get_id()] = v * d_gamma_macro_edge_boundary_value_r[edge.get_id()];
+              d_gamma_flux_r[edge.get_id()] = v * d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 1];
             else
-              d_gamma_flux_l[edge.get_id()] = v * d_gamma_macro_edge_boundary_value_l[edge.get_id()];
+              d_gamma_flux_l[edge.get_id()] = v * d_gamma_macro_edge_boundary_value[2 * edge.get_id() + 0];
           } else {
-            double flux = v * N_in / Q_out * std::abs(Q[i]) * A[i] / std::abs(Q[i]);
+            double flux = v * N_in / Q_out * std::abs(Q_up_values[i]) * A_up_values[i] / std::abs(Q_up_values[i]);
             if (edge.is_pointing_to(v_id))
               d_gamma_flux_r[edge.get_id()] = flux;
             else
@@ -436,28 +345,25 @@ private:
 
   void apply_inverse_mass();
 
+private:
   MPI_Comm d_comm;
 
-  std::vector<double> d_A_macro_edge_boundary_value_l;
-  std::vector<double> d_A_macro_edge_boundary_value_r;
+  std::shared_ptr<GraphStorage> d_graph;
+  std::shared_ptr<DofMap> d_dof_map_flow;
+  std::shared_ptr<DofMap> d_dof_map_transport;
 
-  std::vector<double> d_Q_macro_edge_boundary_value_l;
-  std::vector<double> d_Q_macro_edge_boundary_value_r;
+  FlowUpwindEvaluator d_flow_upwind_evaluator;
 
-  std::vector<double> d_gamma_macro_edge_boundary_value_l;
-  std::vector<double> d_gamma_macro_edge_boundary_value_r;
+  Communicator d_edge_boundary_communicator;
+
+  std::vector<double> d_gamma_macro_edge_boundary_value;
 
   std::vector<double> d_gamma_flux_l;
   std::vector<double> d_gamma_flux_r;
 
   std::vector<double> d_rhs;
   std::vector<double> d_solution;
-
-  std::shared_ptr<GraphStorage> d_graph;
-  std::shared_ptr<DofMap> d_dof_map_flow;
-  std::shared_ptr<DofMap> d_dof_map_transport;
 };
- */
 
 } // namespace macrocirculation
 
