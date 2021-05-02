f6396b1a2f895c19392bf74adfd1fd246c9a8482
Prashant K. Jha <pjha.sci@gmail.com>
2021-05-02 15:25:14 -0500

    add working docker files and install methods

--- tools/script/install/ubuntu/install_petsc_libmesh.sh
+++ tools/script/install/ubuntu/install_petsc_libmesh.sh
@@ -38,22 +38,16 @@ echo "Shell: $(which sh)"
 CMAKE_EXE="cmake"
 
 echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "BOOST"
+echo "apt-get installs"
 echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt-get install libboost-all-dev
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "VTK"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libvtk7-dev
+# sudo apt-get install libboost-all-dev libvtk7-dev libblas-dev liblapack-dev
 
 echo "<<<<<<<<<<< >>>>>>>>>>>"
 echo "PETSC"
 echo "<<<<<<<<<<< >>>>>>>>>>>"
-PETSC_VER="3.12.1"
-PETSC_TAR_FILE="petsc-${PETSC_VER}.tar.gz"
+PETSC_VER="3.13.3"
+PETSC_TAR_FILE="petsc-lite-${PETSC_VER}.tar.gz"
 PETSC_INSTALL_PATH="0" # <<--- fix install path (e.g. /usr/local/)
-PETSC_BUILD_PATH=$BUILDDIR/petsc/$PETSC_VER/$BUILD_TYPE/
 PETSC_SOURCE_DIR=$SOURCEDIR/petsc/$PETSC_VER
 
 if [[ $PETSC_INSTALL_PATH -eq "0" ]]; then
@@ -65,47 +59,28 @@ petsc_build="1"
 if [[ $petsc_build -eq "1" ]]; then
   # download library
   cd $SOURCEDIR
-  if [ ! -f "$PETSC_TAR_FILE" ]; then
-    wget "https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/${PETSC_TAR_FILE}"
-  fi
-
-  if [ ! -d "$PETSC_SOURCE_DIR" ]; then
-    mkdir -p $PETSC_SOURCE_DIR
-    tar -zxf $PETSC_TAR_FILE -C $PETSC_SOURCE_DIR --strip-components=1
-  fi
+  wget "https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/${PETSC_TAR_FILE}"
+  
+  mkdir -p $PETSC_SOURCE_DIR
+  tar -zxf $PETSC_TAR_FILE -C $PETSC_SOURCE_DIR --strip-components=1
 
   # build library
-  cd $BUILDDIR
+  cd $PETSC_SOURCE_DIR
+  ./configure --prefix=$PETSC_INSTALL_PATH \
+              --with-blas-lib=libblas.a \
+              --with-lapack-lib=liblapack.a \
+              --download-hypre  \
+              --download-scalapack \
+              --download-mumps \
+              --download-metis \
+              --download-parmetis \
+              --download-superlu \
+              --download-superlu_dist \
+              --with-debugging=0 \
+              COPTFLAGS='-O3' CXXOPTFLAGS='-O3' FOPTFLAGS='-O3' 
 
-  petsc_arch="arch-linux2-c-debug"
-  if [ ! -d "$PETSC_BUILD_PATH" ]; then
-    mkdir -p "$PETSC_BUILD_PATH"
-
-    cd "$PETSC_BUILD_PATH"
-
-    ${PETSC_SOURCE_DIR}/configure --prefix="$PETSC_INSTALL_PATH" \
-                --COPTFLAGS='-O3' \
-                --CXXOPTFLAGS='-O3' \
-                --FOPTFLAGS='-O3' \
-                --download-fblaslapack=1 \
-                --with-mumps=1 --download-mumps=1 \
-                --with-metis=1 --download-metis=1 \
-                --with-mumps=1 --download-mumps=1 \
-                --with-blacs=1 --download-blacs=1 \
-                --with-hypre=1 --download-hypre=1 \
-                --with-parmetis=1 --download-parmetis=1 \
-                --with-scalapack=1 --download-scalapack=1 \
-                --with-superlu_dist=1 --download-superlu_dist=1 \
-                --with-superlu=1 --download-superlu=1 \
-                --download-hdf5=ifneeded
-  fi
-
-  cd "$PETSC_BUILD_PATH"
-
-  make PETSC_DIR="$PETSC_BUILD_PATH" PETSC_ARCH="$petsc_arch" all -j 14
-  make PETSC_DIR="$PETSC_BUILD_PATH" PETSC_ARCH="$petsc_arch" install
-  make PETSC_DIR="$PETSC_INSTALL_PATH" PETSC_ARCH="" test
-  make PETSC_DIR="$PETSC_INSTALL_PATH" PETSC_ARCH="" streams
+  make VERBOSE=1 PETSC_DIR=`pwd` -j $BUILDTHREADS all
+  make PETSC_DIR=`pwd` install 
 fi
 
 echo "<<<<<<<<<<< >>>>>>>>>>>"
@@ -114,7 +89,6 @@ echo "<<<<<<<<<<< >>>>>>>>>>>"
 LIBMESH_VER="1.5.0"
 LIBMESH_TAR_FILE="v$LIBMESH_VER.tar.gz"
 LIBMESH_INSTALL_PATH="0" # <<--- fix install path (e.g. /usr/local/)
-LIBMESH_BUILD_PATH=$BUILDDIR/libmesh/$LIBMESH_VER/$BUILD_TYPE/
 LIBMESH_SOURCE_DIR=$SOURCEDIR/libmesh/$LIBMESH_VER
 
 if [[ $LIBMESH_INSTALL_PATH -eq "0" ]]; then
@@ -126,30 +100,19 @@ libmesh_build="1"
 if [[ $libmesh_build -eq "1" ]]; then
   # download library
   cd $SOURCEDIR
-  if [ ! -f "$LIBMESH_TAR_FILE" ]; then
-    wget "https://github.com/libMesh/libmesh/archive/refs/tags/$LIBMESH_TAR_FILE"
-  fi
+  wget "https://github.com/libMesh/libmesh/archive/refs/tags/$LIBMESH_TAR_FILE"
 
-  if [ ! -d "$LIBMESH_SOURCE_DIR" ]; then
-    mkdir -p $LIBMESH_SOURCE_DIR
-    tar -zxf $LIBMESH_TAR_FILE -C $LIBMESH_SOURCE_DIR --strip-components=1
-  fi
+  mkdir -p $LIBMESH_SOURCE_DIR
+  tar -zxf $LIBMESH_TAR_FILE -C $LIBMESH_SOURCE_DIR --strip-components=1
 
   # build library
-  cd $BUILDDIR
-
-  if [ ! -d "$LIBMESH_BUILD_PATH" ]; then
-    mkdir -p "$LIBMESH_BUILD_PATH"
-
-    cd "$LIBMESH_BUILD_PATH"
-
-    export PETSC_DIR="$PETSC_INSTALL_PATH"
-    unset PETSC_ARCH
-    ${LIBMESH_SOURCE_DIR}/configure --prefix="$LIBMESH_INSTALL_PATH" \
-                --with-metis=PETSc
-  fi
-  cd "$LIBMESH_BUILD_PATH"
-  make -j -l$BUILDTHREADS
+  cd "$LIBMESH_SOURCE_DIR"
+  export PETSC_DIR="$PETSC_INSTALL_PATH"
+  ./configure --with-methods="opt" \
+              --prefix="$LIBMESH_INSTALL_PATH" \
+              --with-metis=PETSc
+  
+  make -j $BUILDTHREADS
   make install
   make check
 fi
