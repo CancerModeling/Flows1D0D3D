793b012b0a2ddb4b7d710eecf991d6fd6e27a017
Prashant K. Jha <pjha.sci@gmail.com>
2021-05-06 14:37:52 -0500

    minor update...ctest works on ubuntu

--- tools/script/install/ubuntu/build.sh
+++ tools/script/install/ubuntu/build.sh
@@ -6,9 +6,6 @@
 ## Set the paths (see below) where libmesh and petsc are installed 
 ## Run: ./build.sh 
 
-MY_PWD=$pwd
-local="${HOME}/Softwares/local_libmesh"
-
 # libraries (set paths appropriately)
 libmesh="0"
 petsc="0"
@@ -18,13 +15,14 @@ if [[ $libmesh -eq "0" -or $petsc -eq "0" ]]; then
 fi
 
 # build
-target_build=$(pwd)
 source="../../."
 cmake -DLIBMESH_DIR="$libmesh" \
 			-DPETSC_LIB="$petsc/lib" \
 			-DCMAKE_BUILD_TYPE=Release \
 			-DCMAKE_C_COMPILER=/usr/bin/gcc \
-			-DCMAKE_CXX_COMPILER=/usr/bin/g++
+			-DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+  	  -DLIBTG_BUILD_FLAG=Build_NetFVFE \
+  	  -DEnable_Tests=ON \
 			"$source"
 
 make -j 4
