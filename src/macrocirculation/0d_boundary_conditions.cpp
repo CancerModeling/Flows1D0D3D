3e7409b1a8646e641a4fe02c2b61525e9f9c88a0
koepplto <koepplto@ma.tum.de>
2021-07-15 16:12:57 +0200

    Updated weights

--- src/macrocirculation/0d_boundary_conditions.cpp
+++ src/macrocirculation/0d_boundary_conditions.cpp
@@ -86,12 +86,14 @@ void convert_rcr_to_partitioned_tree_bcs(const std::shared_ptr<GraphStorage> &gr
       std::vector<double> list_R;
 
       // arterioles
-      list_C.push_back(C*0.07);
-      list_C.push_back(C*0.06);
-      list_C.push_back(C*0.05);
-      list_C.push_back(C*0.04);
-      list_C.push_back(C*0.02);
-      list_C.push_back(C*0.02);
+      list_C.push_back(C*0.08);
+      list_C.push_back(C*0.09);
+      list_C.push_back(C*0.10);
+      list_C.push_back(C*0.10);
+      list_C.push_back(C*0.11);
+      list_C.push_back(C*0.12);
+      list_C.push_back(C*0.48);
+      list_C.push_back(C*0.68);
       // capillaries
       list_C.push_back(C*0.14);
       // venules
@@ -100,18 +102,20 @@ void convert_rcr_to_partitioned_tree_bcs(const std::shared_ptr<GraphStorage> &gr
       list_C.push_back(C*0.40);
 
       // arterioles 60
-      list_R.push_back(R2*0.05);
+      list_R.push_back(R2*0.10);
+      list_R.push_back(R2*0.20);
       list_R.push_back(R2*0.15);
       list_R.push_back(R2*0.15);
-      list_R.push_back(R2*0.20);
-      list_R.push_back(R2*0.20);
+      list_R.push_back(R2*0.15);
+      list_R.push_back(R2*0.10);
+      list_R.push_back(R2*0.10);
       list_R.push_back(R2*0.05);
       // capillaries
-      list_R.push_back(R2*0.10);
+      list_R.push_back(R2*0.05);
       // venules
-      list_R.push_back(R2*0.05);
+      list_R.push_back(R2*0.025);
       // small veins
-      list_R.push_back(R2*0.05);
+      list_R.push_back(R2*0.025);
 
       vertex.set_to_vessel_tree_outflow(p_cap, list_R, list_C, 1);
     }
