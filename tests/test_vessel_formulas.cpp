c7a4f406b1ea0d0f99fd8de472466d0652ccf0cf
wagnandr <wagneran@ma.tum.de>
2021-10-22 15:26:01 +0200

    adds test for inflow boundary conditions

--- tests/test_vessel_formulas.cpp
+++ tests/test_vessel_formulas.cpp
@@ -29,3 +29,32 @@ TEST_CASE("StaticPressureAndAreaAreInverse", "[VesselFormulas]") {
 
   REQUIRE(recreated_A == Approx(A).epsilon(1e-16));
 }
+
+TEST_CASE("PressureAndFluxInflowBCAreConsistent", "[VesselFormulas]") {
+  const double radius = 1.2;
+  const double wall_thickness = 0.163;
+  const double elastic_modulus = 400000.0;
+  const double A0 = std::pow(radius, 2) * M_PI;
+
+  const double G0 = mc::calculate_G0(wall_thickness, elastic_modulus, A0);
+  const double rho = 1.028e-3;
+
+  const double Q_DG = 10.;
+  const double A_DG = 5.;
+
+  const double Q_up = 12.;
+
+  {
+    const double A_up = mc::nonlinear::inflow::get_upwinded_A_from_Q(Q_DG, A_DG, true, Q_up, G0, rho, A0);
+    const double recreated_Q_up = mc::nonlinear::inflow::get_upwinded_Q_from_A(Q_DG, A_DG, +1., A_up, G0, rho, A0);
+
+    REQUIRE(recreated_Q_up == Approx(Q_up).epsilon(1e-8));
+  }
+
+  {
+    const double A_up = mc::nonlinear::inflow::get_upwinded_A_from_Q(-Q_DG, A_DG, false, -Q_up, G0, rho, A0);
+    const double recreated_Q_up = mc::nonlinear::inflow::get_upwinded_Q_from_A(-Q_DG, A_DG, -1., A_up, G0, rho, A0);
+
+    REQUIRE(recreated_Q_up == Approx(-Q_up).epsilon(1e-8));
+  }
+}
