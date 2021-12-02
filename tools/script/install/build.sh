0befcdf946dc8db75e9f6133fb1b102a0a31a01d
wagnandr <wagneran@ma.tum.de>
2021-12-02 19:38:44 +0100

    removing libmesh library

--- tools/script/install/build.sh
+++ tools/script/install/build.sh
@@ -3,21 +3,19 @@
 ## Usage
 ## Create directory <path to TumorModels>/build/test and cd to it
 ## Copy this script to <path to TumorModels>/build/test
-## Set the paths (see below) where libmesh and petsc are installed 
+## Set the path (see below) where petsc is installed
 ## Run: ./build.sh 
 
 # libraries (set paths appropriately)
-libmesh="0"
 petsc="0"
-if [[ $libmesh -eq "0" -or $petsc -eq "0" ]]; then
-  echo "Specify the paths where libmesh and petsc are installed."
+if [[ $petsc -eq "0" ]]; then
+  echo "Specify the path where petsc is installed."
   exit
 fi
 
 # build
 source="../../."
-cmake -DLIBMESH_DIR="$libmesh" \
-			-DPETSC_DIR="$petsc" \
+cmake -DPETSC_DIR="$petsc" \
 			-DCMAKE_BUILD_TYPE=Release \
   	  -DEnable_Tests=ON \
 			"$source"
