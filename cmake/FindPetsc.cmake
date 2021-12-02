6d475ada3ee5bff9d278598c80bd089c270ff2fd
wagnandr <wagneran@ma.tum.de>
2021-12-02 18:50:06 +0100

    wip removing libmesh dependency

--- cmake/FindPetsc.cmake
+++ cmake/FindPetsc.cmake
@@ -7,12 +7,7 @@ find_package(PkgConfig)
 set(PETSC_DIR $ENV{PETSC_DIR} CACHE PATH "Petsc installation directory")
 set(ENV{PKG_CONFIG_PATH} "${PETSC_DIR}/lib/pkgconfig")
 
-find_library(PETSC_LIBRARIES
-        NAMES libpetsc.so libpetsc.dylib
-        HINTS "${PETSC_DIR}/lib/")
-#        HINTS /usr/lib64 /usr/local/lib64 /usr/lib/ /usr/local/lib
-#"${PETSC_DIR}/lib/")
-
+pkg_check_modules(PETSC REQUIRED IMPORTED_TARGET PETSc)
 
 if (NOT PETSC_LIBRARIES)
     message(FATAL_ERROR "PETSC_LIBRARIES Library not found: Specify the PETSC_DIR where petsc is located")
