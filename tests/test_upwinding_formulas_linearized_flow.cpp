72eb0899e179da0a8473c1be22c215093fc2453d
wagnandr <wagneran@ma.tum.de>
2021-12-14 14:22:21 +0100

    adds tests for comparing linearized and nonlinear flows

--- tests/test_upwinding_formulas_linearized_flow.cpp
+++ tests/test_upwinding_formulas_linearized_flow.cpp
@@ -8,110 +8,49 @@
 #include "catch2/catch.hpp"
 #include <utility>
 
+#include "macrocirculation/upwinding_formulas_linearized_flow.hpp"
 #include "macrocirculation/vessel_formulas.hpp"
 
 namespace mc = macrocirculation;
 
-TEST_CASE("StaticPressureAndAreaAreInverse", "[VesselFormulas]") {
-  double radius = 1.2;
-  double wall_thickness = 0.163;
-  double elastic_modulus = 400000.0;
-  double A0 = std::pow(radius, 2) * M_PI;
 
-  const double G0 = mc::calculate_G0(wall_thickness, elastic_modulus, A0);
-  const double rho = 1.028e-3;
-
-  const double A = 5.;
-
-  const mc::VesselParameters params (G0, A0, rho);
-
-  const double recreated_A = mc::nonlinear::get_A_from_p(params, mc::nonlinear::get_p_from_A(params, A));
-
-  REQUIRE(recreated_A == Approx(A).epsilon(1e-16));
-}
-
-TEST_CASE("PressureAndFluxInflowBCAreConsistent", "[VesselFormulas]") {
-  const double radius = 1.2;
-  const double wall_thickness = 0.163;
+TEST_CASE("InnerBoundaryAndNfurcationBoundaryConincide", "[UpwindingFormulasLinearizedFlow]") {
+  const double radius = 0.403;
+  const double wall_thickness = 0.067;
   const double elastic_modulus = 400000.0;
-  const double A0 = std::pow(radius, 2) * M_PI;
-
-  const double G0 = mc::calculate_G0(wall_thickness, elastic_modulus, A0);
   const double rho = 1.028e-3;
 
-  const double Q_DG = 10.;
-  const double A_DG = 5.;
+  const double A0 = std::pow(radius, 2) * M_PI;
+  const double G0 = mc::calculate_G0(wall_thickness, elastic_modulus, A0);
 
-  const double Q_up = 12.;
+  auto pdata = mc::VesselParameters(G0, A0, rho) ;
 
-  {
-    const double A_up = mc::nonlinear::inflow::get_upwinded_A_from_Q(Q_DG, A_DG, true, Q_up, {G0, A0, rho});
-    const double recreated_Q_up = mc::nonlinear::inflow::get_upwinded_Q_from_A(Q_DG, A_DG, +1., A_up, {G0, A0, rho});
+  const double C = mc::linear::get_C(pdata);
+  const double L = mc::linear::get_L(pdata);
 
-    REQUIRE(recreated_Q_up == Approx(Q_up).epsilon(1e-8));
-  }
+  double alpha = std::sqrt(C/L);
 
-  {
-    const double A_up = mc::nonlinear::inflow::get_upwinded_A_from_Q(-Q_DG, A_DG, false, -Q_up, {G0, A0, rho});
-    const double recreated_Q_up = mc::nonlinear::inflow::get_upwinded_Q_from_A(-Q_DG, A_DG, -1., A_up, {G0, A0, rho});
+  double p_l = 100;
+  double p_r = 105;
+  double q_l = 0.15;
+  double q_r = 0.10;
+  double p_up = 0;
+  double q_up = 0;
 
-    REQUIRE(recreated_Q_up == Approx(-Q_up).epsilon(1e-8));
-  }
-}
+  mc::linearized::inner_boundary(alpha, p_l, q_l, p_r, q_r, p_up, q_up);
 
-TEST_CASE("PiecewiseLinearSourceFunctionsWorksCorrectly", "[VesselFormulas]") {
-  {
-    mc::piecewise_linear_source_function f({0, 1, 2, 3, 4}, {0.5, 1.5, 2.5, 3.5, 4.5});
+  const std::vector< double > p_vec = {p_l, p_r};
+  const std::vector< double > q_vec = {q_l, q_r};
+  std::vector< double > p_up_vec = {0, 0};
+  std::vector< double > q_up_vec = {0, 0};
 
-    // test edges
-    REQUIRE(f(0) == Approx(0.5).epsilon(1e-16));
-    REQUIRE(f(4) == Approx(4.5).epsilon(1e-16));
-    REQUIRE(f(4+1e-16) == Approx(4.5).epsilon(1e-16));
-    // test intermediate values
-    REQUIRE(f(0.5) == Approx(1).epsilon(1e-16));
-    REQUIRE(f(1.) == Approx(1.5).epsilon(1e-16));
-    REQUIRE(f(1.+1e-16) == Approx(1.5+1e-16).epsilon(1e-16));
-    REQUIRE(f(2.5) == Approx(3.).epsilon(1e-16));
-    REQUIRE(f(3.5) == Approx(4.).epsilon(1e-16));
-  }
+  // const std::vector< mc::VesselParameters> params = { {}, physical_data };
 
-  {
-    mc::piecewise_linear_source_function f({1, 2, 3, 4}, {1.5, 2.5, 3.5, 4.5});
-
-    REQUIRE(f(4) == Approx(4.5).epsilon(1e-16));
-    REQUIRE(f(4+1e-16) == Approx(4.5).epsilon(1e-16));
-    REQUIRE(f(1.) == Approx(1.5).epsilon(1e-16));
-    REQUIRE(f(1.+1e-16) == Approx(1.5+1e-16).epsilon(1e-16));
-    REQUIRE(f(2.5) == Approx(3.).epsilon(1e-16));
-    REQUIRE(f(3.5) == Approx(4.).epsilon(1e-16));
-  }
-}
-
-TEST_CASE("PeriodicPiecewiseLinearSourceFunctionsWorksCorrectly", "[VesselFormulas]") {
-  mc::piecewise_linear_source_function f({1, 2, 3, 4}, {1.5, 2.5, 3.5, 4.5}, true);
+  mc::linearized::nfurcation_boundary(p_vec, q_vec, {pdata, pdata}, {+1, -1}, p_up_vec, q_up_vec);
 
   // test edges
-  REQUIRE(f(1+1e-12) == Approx(1.5).epsilon(1e-12));
-  REQUIRE(f(1.5) == Approx(2.).epsilon(1e-12));
-  REQUIRE(f(2) == Approx(2.5).epsilon(1e-12));
-  REQUIRE(f(2.5) == Approx(3.).epsilon(1e-12));
-  REQUIRE(f(3) == Approx(3.5).epsilon(1e-12));
-  REQUIRE(f(3.5) == Approx(4).epsilon(1e-12));
-  REQUIRE(f(4-1e-12) == Approx(4.5).epsilon(1e-12));
-  REQUIRE(f(4.2) == Approx(1.7).epsilon(1e-12));
-  REQUIRE(f(4.5) == Approx(2.).epsilon(1e-12));
-}
-TEST_CASE("Upwinding", "[VesselFormulas]") {
-  mc::piecewise_linear_source_function f({1, 2, 3, 4}, {1.5, 2.5, 3.5, 4.5}, true);
-
-  // test edges
-  REQUIRE(f(1+1e-12) == Approx(1.5).epsilon(1e-12));
-  REQUIRE(f(1.5) == Approx(2.).epsilon(1e-12));
-  REQUIRE(f(2) == Approx(2.5).epsilon(1e-12));
-  REQUIRE(f(2.5) == Approx(3.).epsilon(1e-12));
-  REQUIRE(f(3) == Approx(3.5).epsilon(1e-12));
-  REQUIRE(f(3.5) == Approx(4).epsilon(1e-12));
-  REQUIRE(f(4-1e-12) == Approx(4.5).epsilon(1e-12));
-  REQUIRE(f(4.2) == Approx(1.7).epsilon(1e-12));
-  REQUIRE(f(4.5) == Approx(2.).epsilon(1e-12));
+  REQUIRE(p_up_vec[0] == Approx(p_up_vec[1]).epsilon(1e-12));
+  REQUIRE(q_up_vec[0] == Approx(q_up_vec[1]).epsilon(1e-12));
+  REQUIRE(p_up_vec[0] == Approx(p_up).epsilon(1e-12));
+  REQUIRE(q_up_vec[0] == Approx(q_up).epsilon(1e-12));
 }
